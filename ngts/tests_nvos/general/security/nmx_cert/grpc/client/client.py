907033e8e14f24148019ef475653c1e5bdfef1dc
Alon Navarro <alonn@nvidia.com>
2024-07-13 17:17:24 +0300

    [nvos][nmx-cert] add grpc client & server
    - got proto definition from nmx
    - add client to be used in tests with real nmx-c
    - add server to mock server to test the client locally
    
    Change-Id: Ie349546db2c7b635640cd8ce4102c605cc2d3ef7

---
+++ ngts/tests_nvos/general/security/nmx_cert/grpc/client/client.py
@@ -0,0 +1,89 @@
+import time
+
+import grpc
+
+import ngts.tests_nvos.general.security.nmx_cert.grpc.proto.nmx_m_nmx_c_pb2 as pb
+import ngts.tests_nvos.general.security.nmx_cert.grpc.proto.nmx_m_nmx_c_pb2_grpc as pb_grpc
+from ngts.tests_nvos.general.security.nmx_cert.constants import EncryptionMode
+from ngts.tests_nvos.general.security.nmx_cert.grpc.config import CONFIG, GrpcConfig
+from ngts.tests_nvos.general.security.nmx_cert.grpc.utils.logs import standalone_logger
+
+
+class ClientApp:
+    def __init__(self, config: GrpcConfig, logger=standalone_logger):
+        self.name = 'CLIENT'
+        self.config = config
+        self.logger = logger or standalone_logger
+
+    def run(self) -> str:
+        responses = []
+
+        self._log(f'starting client\n{self.config}')
+        # Create a channel to the server
+        with self._get_channel() as channel:
+            self._log('created channel')
+
+            # Create a stub (client)
+            client = pb_grpc.NMX_ControllerStub(channel)
+            self._log('created grpc client')
+
+            for i in range(self.config.client.num_requests):
+                # Create a request message
+                request = pb.ClientHello(
+                    gatewayId=str(i),
+                    major_version=pb.ProtoMsgMajorVersion.PROTO_MSG_MAJOR_VERSION_UNDEFINED,
+                    minor_version=pb.ProtoMsgMajorVersion.PROTO_MSG_MAJOR_VERSION_UNDEFINED
+                )
+                self._log('created request message')
+
+                # Make the call to the server
+                self._log('send grpc request to server: Hello()')
+                response = client.Hello(request)
+                self._log(f"response from server:\n{response}")
+                responses.append(str(response))
+
+                self._log(f'sleep {self.config.client.delay_between_requests} seconds')
+                time.sleep(self.config.client.delay_between_requests)
+
+        return '\n'.join(responses)
+
+    def _get_channel(self):
+        dial_to = f'{self.config.server.address}:{self.config.server.port}'
+        if self.config.client.tls_mode == EncryptionMode.NONE:
+            return grpc.insecure_channel(dial_to)
+        else:
+            return grpc.secure_channel(dial_to, self._get_client_ssl_config())
+
+    def _get_client_ssl_config(self):
+        with open(self.config.client.cacert.cacert, 'rb') as f:
+            cacert = f.read()
+        with open(self.config.client.cert.private, 'rb') as f:
+            private = f.read()
+        with open(self.config.client.cert.public, 'rb') as f:
+            public = f.read()
+
+        if self.config.client.tls_mode == EncryptionMode.TLS:
+            credentials = grpc.ssl_channel_credentials(
+                root_certificates=cacert,
+            )
+        elif self.config.client.tls_mode == EncryptionMode.MTLS:
+            credentials = grpc.ssl_channel_credentials(
+                root_certificates=cacert,
+                private_key=private,
+                certificate_chain=public
+            )
+        else:
+            credentials = None
+
+        return credentials
+
+    def _log(self, msg: str):
+        self.logger.info(f'[{self.name}] {msg}')
+
+
+def run_grpc_client_app(config: GrpcConfig, logger=None) -> str:
+    return ClientApp(config, logger).run()
+
+
+if __name__ == '__main__':
+    run_grpc_client_app(CONFIG)
---
+++ ngts/tests_nvos/general/security/nmx_cert/grpc/server/NMX_ControllerServicer.py
@@ -0,0 +1,34 @@
+import ngts.tests_nvos.general.security.nmx_cert.grpc.proto.nmx_m_nmx_c_pb2 as pb
+import ngts.tests_nvos.general.security.nmx_cert.grpc.proto.nmx_m_nmx_c_pb2_grpc as pb_grpc
+from ngts.tests_nvos.general.security.nmx_cert.grpc.config import GrpcConfig
+from ngts.tests_nvos.general.security.nmx_cert.grpc.utils.logs import standalone_logger
+
+
+class NMX_ControllerServicer(pb_grpc.NMX_ControllerServicer):
+    def __init__(self, name: str, config: GrpcConfig, logger=standalone_logger):
+        self.name = name
+        self.config = config
+        self.counter = 0
+        self.logger = logger
+
+    def Hello(self, request, context):
+        self.counter += 1
+
+        self._log(f'received Hello() request #{self.counter} from {context.peer()}')
+
+        return pb.ServerHello(
+            serverHeader=pb.ServerHeader(
+                domain_uuid=f"Hello World! {self.counter}",
+                app_uuid=f"Hello World! {self.counter}",
+                app_ver=f"Hello World! {self.counter}",
+                returnCode=pb.ST_ReturnCode.ST_UNDEFINED
+            ),
+            components_ver=[],
+            capabilities=[],
+            host_os_details=f"Hello World! {self.counter}",
+            major_version=pb.ProtoMsgMajorVersion.PROTO_MSG_MAJOR_VERSION_UNDEFINED,
+            minor_version=pb.ProtoMsgMajorVersion.PROTO_MSG_MAJOR_VERSION_UNDEFINED
+        )
+
+    def _log(self, msg: str):
+        self.logger.info(f'[{self.name}] {msg}')
