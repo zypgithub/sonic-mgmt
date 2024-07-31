c74fbbeae4e8f11b24b2b62f156bc8a1549edebd
Nana He <nhe@nvidia.com>
2024-07-31 19:14:56 +0800

    Add WA to copy the hwsku V256 for the moose setup
    Change-Id: I763395bfd644e96f6dd7f80634be6b7bccca49ba

--- ngts/scripts/sonic_deploy/sonic_only_methods.py
+++ ngts/scripts/sonic_deploy/sonic_only_methods.py
@@ -78,8 +78,8 @@ class SonicInstallationSteps:
         add_topo_cmd = SonicInstallationSteps.get_add_topology_cmd(setup_name, dut_name, sonic_topo, neighbor_type, ptf_tag)
         run_background_process_on_host(threads_dict, 'add_topology', add_topo_cmd, timeout=3600, exec_path=ansible_path)
         if not is_bf_topo(sonic_topo) and not is_dualtor_topo(sonic_topo) and "mtvr-hippo-03" != dut_name and\
-                "mtvr-hippo-02" != dut_name and 'bobcat' not in dut_name and "r-leopard-01" != dut_name and \
-                "r-leopard-58" != dut_name:
+                "mtvr-hippo-02" != dut_name and 'bobcat' not in dut_name and "r-leopard-01" != dut_name \
+                and "r-leopard-58" != dut_name and "r-moose-01" != dut_name and "mtvr-moose-04" != dut_name:
             gen_mg_cmd = get_generate_minigraph_cmd(setup_info, dut_name, sonic_topo, port_number)
             run_background_process_on_host(threads_dict, 'generate_minigraph', gen_mg_cmd, timeout=300,
                                            exec_path=ansible_path)
@@ -427,34 +427,42 @@ class SonicInstallationSteps:
         sonic_user = os.getenv("SONIC_SWITCH_USER")
         sonic_password = os.getenv("SONIC_SWITCH_PASSWORD")
         dut_engine = topology_obj.players['dut']['engine']
-        # TODO: This is a WA before the hwsku Mellanox-SN4700-O32 and Mellanox-SN4700-V64 are merged to upstream
+        logger.info(f'Current hwsku in the platform_params is: {platform_params["platform"]}')
+        hwskus = []
+        need_gen_mingraph = False
+        if "mtvr-hippo-03" in setup_name or "mtvr-hippo-02" in setup_name:
+            hwskus = [platform_params["hwsku"]]
+            need_gen_mingraph = True
         if "r-leopard-01" in setup_name or "r-leopard-58" in setup_name:
-            dut_name = setup_info['duts'][0]['dut_name']
-            dut_hwsku_path_o32 = '/usr/share/sonic/device/x86_64-kvm_x86_64-r0/Mellanox-SN4700-O32'
-            sonic_mgmt_hwsku_path = '/usr/share/sonic/device/x86_64-kvm_x86_64-r0'
-            sonic_user = os.getenv("SONIC_SWITCH_USER")
-            sonic_password = os.getenv("SONIC_SWITCH_PASSWORD")
+            hwskus = ['Mellanox-SN4700-O32']
+            need_gen_mingraph = True
+        if "r-moose-01" in setup_name or "mtvr-moose-04" in setup_name:
+            hwskus = ['Mellanox-SN5600-V256']
+            need_gen_mingraph = True
+        if "bobcat" in setup_name:
+            hwskus = ['ACS-SN4280', 'Mellanox-SN4280-O28']
+            need_gen_mingraph = True
+
+        for hwsku in hwskus:
+            if os.path.exists(f'{sonic_mgmt_hwsku_path}/{hwsku}'):
+                logger.warning(f"The hwsku {hwsku} already exist in the sonic mgmt docker, no need to copy")
             execute_script(f'sshpass -p "{sonic_password}" scp -o "StrictHostKeyChecking no"'
-                           f' -r {sonic_user}@{dut_name}:{dut_hwsku_path_o32} '
+                           f' -r {sonic_user}@{dut_name}:{dut_platform_path}/{hwsku} '
                            f'{sonic_mgmt_hwsku_path}', ansible_path)
-            generate_minigraph(ansible_path, setup_info, dut_name, sonic_topo, None)
-        elif "mtvr-hippo-03" in setup_name or "mtvr-hippo-02" in setup_name:
+
+            logger.info(f"Copied the hwsku {hwsku} to sonic-mgmt")
+
+        if "hippo" in setup_name:
             SonicInstallationSteps.remove_redundant_service_port(dut_platform_path, platform_params['hwsku'],
                                                                  dut_engine, cli.cli_obj)
-            dut_engine.run_cmd(f'sudo sonic-cfggen --preset t1 -p -H -k {platform_params["hwsku"]} > '
-                               f'{SonicConst.SONIC_CONFIG_FOLDER}{SonicConst.CONFIG_DB_JSON}')
-            execute_script(f'sshpass -p "{sonic_password}" scp -o "StrictHostKeyChecking no"'
-                           f' -r {sonic_user}@{dut_name}:{dut_platform_path}/{platform_params["hwsku"]} '
-                           f'{sonic_mgmt_hwsku_path}', ansible_path)
-            generate_minigraph(ansible_path, setup_info, dut_name, sonic_topo, None)
-        # TODO: Remove this after SONiC bobcat code is merged to master
-        elif "bobcat" in setup_name:
-            dut_hwsku_paths = ['/usr/share/sonic/device/x86_64-kvm_x86_64-r0/ACS-SN4280',
-                               '/usr/share/sonic/device/x86_64-kvm_x86_64-r0/Mellanox-SN4280-O28']
-            for path in dut_hwsku_paths:
-                execute_script(f'sshpass -p "{sonic_password}" scp -o "StrictHostKeyChecking no"'
-                               f' -r {sonic_user}@{dut_name}:{path} '
-                               f'{sonic_mgmt_hwsku_path}', ansible_path)
+            dut_engine.run_cmd(
+                f"sudo sonic-cfggen --preset t1 -p -H "
+                f"-k {platform_params['hwsku']} > {SonicConst.SONIC_CONFIG_FOLDER}{SonicConst.CONFIG_DB_JSON}")
+
+        if "r-moose-01" in setup_name or "mtvr-moose-04" in setup_name:
+            execute_script(f'sed -i "s/200000/100000/g" {sonic_mgmt_hwsku_path}/Mellanox-SN5600-V256/port_config.ini',
+                           ansible_path)
+        if need_gen_mingraph:
             generate_minigraph(ansible_path, setup_info, dut_name, sonic_topo, None)
 
         cli.enable_async_route_feature(platform_params['platform'], platform_params['hwsku'])
