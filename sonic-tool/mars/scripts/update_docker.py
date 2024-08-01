5f6aa23e0416396f85c45ea23299439f9633f05e
Alon Navarro <alonn@nvidia.com>
2024-08-01 17:59:11 +0300

    [nvos] update devts docker ngts tag from 1.2.329 to 1.2.330
    - install openssl package on sonic-mgmt player venv
    
    Change-Id: Ibbd84da3dcc4032dca6a5b7b8b208039c4ed7404

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -398,7 +398,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.329'}
+    default_list = {'docker-ngts': '1.2.330'}
     return default_list.get(docker_name, latest)
 
 
