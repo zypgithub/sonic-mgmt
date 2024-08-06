3bb6c757be237e26ad016fcd79922c5ae45294f1
Alon Navarro <alonn@nvidia.com>
2024-08-06 15:51:32 +0300

    [nvos][security] fix factory reset gnmi check function
    Change-Id: I672a4973f67d30a533c48e5a911d5905e02c4520

--- ngts/tests_nvos/system/gnmi/helpers.py
+++ ngts/tests_nvos/system/gnmi/helpers.py
@@ -454,14 +454,21 @@ def verify_server_reflection(test_flow, client, skip_cert_verify, err_msg_to_che
         verify_msg_in_out_or_err(err_msg_to_check, out_reflect, err_reflect)
 
 
+def get_scp_player(engines) -> LinuxSshEngine:
+    return engines.sonic_mgmt
+    # return LinuxSshEngine(ip='10.237.116.70', username='root', password='12345')
+
+
 def factory_reset_gnmi_check():
+    engines = TestToolkit.engines
     cert = TestCert.cert_valid_1
     system = System()
-    dut_engine = TestToolkit.engines.dut
+    scp_engine = get_scp_player(engines)
+    dut_engine = engines.dut
     with allure.step(f'before factory reset - import and load certificate "{cert.name}" to gnmi'):
         with allure.step(f'import cert {cert.name}'):
             system.security.certificate.cert_id[cert.name].action_import(
-                uri_bundle=generate_scp_uri_using_player(dut_engine, cert.p12_bundle),
+                uri_bundle=generate_scp_uri_using_player(scp_engine, cert.p12_bundle),
                 passphrase=cert.p12_password).verify_result()
         with allure.step(f'set certificate "{cert.name}" to gnmi'):
             system.gnmi_server.set(CERTIFICATE, cert.name, apply=True).verify_result()
