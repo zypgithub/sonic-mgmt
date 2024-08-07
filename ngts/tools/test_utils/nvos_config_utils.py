14f9d1c17c7aacdb0ee57f78d589c69b94956cfe
rowaida mahameed <rmahameed@nvidia.com>
2024-08-07 16:48:12 +0300

    NVOS - update the way we apply new configuration and clean up the pending list
    Change-Id: Ic6fe27cb78f1910aa334977b59c5bbd4ff7a6148

--- ngts/tools/test_utils/nvos_config_utils.py
+++ ngts/tools/test_utils/nvos_config_utils.py
@@ -134,7 +134,7 @@ def clear_conf(dut_engine, markers=None, set_base_config_function=set_base_confi
                     set_base_config_function(dut_engine=dut_engine, apply=False)
 
             with allure.step("Apply configurations"):
-                NvueGeneralCli.apply_config(dut_engine, ask_for_confirmation=True)
+                NvueGeneralCli.apply_config(dut_engine, ask_for_confirmation=True, option='--assume-yes')
 
             if should_wait_for_nvued_after_apply:
                 DutUtilsTool.wait_for_nvos_to_become_functional(dut_engine).verify_result()
