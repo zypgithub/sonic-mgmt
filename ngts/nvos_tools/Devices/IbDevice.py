cc46e50e338bfaeea067cb58d41c9a3938545f4a
Tamuz Ofer <tofer@nvidia.com>
2024-08-14 12:17:38 +0300

    fix merge mistake
    Change-Id: Ife6c5403be30958513a27d94e19768896b72f220

--- ngts/nvos_tools/Devices/IbDevice.py
+++ ngts/nvos_tools/Devices/IbDevice.py
@@ -1303,16 +1303,6 @@ class JulietNonScaleoutSwitch(JulietScaleoutSwitch):
                                                       "model": ExpectedString(regex="692-9K36F-00MV-JS0")})
 
 
-# -------------------------- JulietNonScaleout Switch ----------------------------
-class JulietNonScaleoutSwitch(JulietScaleoutSwitch):
-
-    def __init__(self):
-        super().__init__()
-
-    def _init_constants(self):
-        super()._init_constants()
-
-
 # -------------------------- Caiman Switch ----------------------------
 
 
