3d0b987d7d6280df17708b23e41abeaaa0119f96
jbao <jbao@nvidia.com>
2024-08-29 11:21:45 +0300

    update snake cases
    1. refactor the snake cases
    2. Add new function to support sonic fanout
    
    Change-Id: Iaf0337a7d8b0afe9e6ccd6fd4c9798d04648887e
    (cherry picked from commit a4086e16ffd0897e4367d08814a992a4871684e7)

--- tests/snake/test_snake.py
+++ tests/snake/test_snake.py
@@ -1,10 +1,15 @@
 import allure
 import pytest
 import time
+import logging
 
 pytestmark = [
-    pytest.mark.topology('t0')
+    pytest.mark.topology('any')
 ]
+
+logger = logging.getLogger(__name__)
+snake_start_ip = "10.5.0.1"
+
 
 def test_snake(duthost, configure_switches):
 
@@ -14,10 +19,19 @@ def test_snake(duthost, configure_switches):
         duthost.command("sudo apt update")
         duthost.command("sudo apt install -y iperf3")
 
+    with allure.step("check if the snake is pingable from end to start"):
+        try:
+            duthost.command(f"sudo ip vrf exec Vrf{last_vrf} ping -c 10 {snake_start_ip}")
+        except Exception as err:
+            logger.warning(f"ping failed error:{err}")
+            logger.warning("try run traceroute to get more debug info")
+            duthost.command(f"sudo ip vrf exec Vrf{last_vrf} traceroute {snake_start_ip}")
+
     with allure.step("Run iperf"):
-        duthost.command("sudo ip vrf exec Vrf0 iperf3 -s -B 10.5.0.1", module_async=True)
+        duthost.command(f"sudo ip vrf exec Vrf0 iperf3 -s -B {snake_start_ip} -D ", module_async=True)
         time.sleep(5)
-        out = duthost.command("sudo ip vrf exec Vrf{} iperf3 -t 60 -i 10 -c 10.5.0.1 --connect-timeout 5000".format(last_vrf))
+        out = duthost.command(
+            f"sudo ip vrf exec Vrf{last_vrf} iperf3 -t 60 -i 10 -c {snake_start_ip} --connect-timeout 5000")
 
         # Apply some objective constraints here for now just check success
         assert "Done" in out["stdout"]
