4040509bd238d7947b50042efe6a1f094c65acb3
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-09-05 14:49:30 +0800

    Optimize the matching rule of conditional marks (#14395)
    What is the motivation for this PR?
    Previously, the matching rule of the conditional marks was based on the longest match. This meant that when a test case had multiple potential matches, we only applied the mark with the longest matching entry. In this PR, we've optimized this rule: we now consider all potential matches. If a mark is unique across these matches, it will be applied to the test case. Otherwise, we will still use the longest matching entry.
    
    How did you do it?
    In this PR, we've optimized this rule of conditional mark: we now consider all potential matches. If a mark is unique across these matches, it will be applied to the test case. Otherwise, we will still use the longest matching entry.
    
    How did you verify/test it?
    We have conditional marks below
    
    mark:
      skip:
        reason: "Skip mark"
        conditions:
          - "topo_type in ['vs']"
    
    mark/test_mark.py:
      xfail:
        reason: "Xfail mark/test_mark.py"
        conditions:
          - "asic_type in ['vs']"
      skip:
        reason: "Skip mark/test_mark.py"
        conditions:
          - "asic_type in ['vs']"
    
    mark/test_mark.py::test_conditional_mark:
      skip:
        reason: "Skip mark/test_mark.py::test_conditional_mark"
        conditions:
          - "asic_type in ['vs']"
    And we run test case mark/test_mark.py::test_conditional_mark on kvm testbed.
    We get the expected match of this test case and add them to the case
    
    08:25:40 __init__.pytest_collection_modifyitems   L0611 INFO   | Found match "[{'mark/test_mark.py::test_conditional_mark': {'skip': {'reason': 'Skip mark/test_mark.py::test_conditional_mark', 'conditions': ["asic_type in ['vs']"]}}}, {'mark/test_mark.py': {'xfail': {'reason': 'Xfail mark/test_mark.py', 'conditions': ["asic_type in ['vs']"]}}}]" for test case "mark/test_mark.py::test_conditional_mark"
    08:25:40 __init__.pytest_collection_modifyitems   L0649 INFO   | Adding mark MarkDecorator(mark=Mark(name='skip', args=(), kwargs={'reason': 'Skip mark/test_mark.py::test_conditional_mark'})) to mark/test_mark.py::test_conditional_mark
    08:25:40 __init__.pytest_collection_modifyitems   L0649 INFO   | Adding mark MarkDecorator(mark=Mark(name='xfail', args=(), kwargs={'reason': 'Xfail mark/test_mark.py', 'strict': False})) to mark/test_mark.py::test_conditional_mark
    Any platform specific information?
    
    Change-Id: Ifaeff9fc26343e9d687cb54f6c5da91f8063c357

--- tests/common/plugins/conditional_mark/__init__.py
+++ tests/common/plugins/conditional_mark/__init__.py
@@ -401,22 +401,32 @@ def load_basic_facts(dut_name, session):
 
     return results
 
+<<<<<<< HEAD
 def find_longest_matches(nodeid, conditions):
     """Find the longest matches of the given test case name in the conditions list.
 
     This is similar to longest prefix match in routing table. The longest match takes precedence.
+=======
+
+def find_all_matches(nodeid, conditions):
+    """Find all matches of the given test case name in the conditions list.
+>>>>>>> d03f0fbdd (Optimize the matching rule of conditional marks (#14395))
 
     Args:
         nodeid (str): Full test case name
         conditions (list): List of conditions
 
     Returns:
-        str: Longest match test case name or None if not found
+        list: All match test case name or None if not found
     """
-    longest_matches = []
+    all_matches = []
     max_length = -1
+    conditional_marks = {}
+    matches = []
+
     for condition in conditions:
         # condition is a dict which has only one item, so we use condition.keys()[0] to get its key.
+<<<<<<< HEAD
         condition_entry = list(condition.keys())[0]
         condition_items = condition[condition_entry]
         if "regex" in condition_items.keys():
@@ -429,6 +439,9 @@ def find_longest_matches(nodeid, conditions):
         else:
             match = nodeid.startswith(condition_entry)
         if match:
+=======
+        if nodeid.startswith(list(condition.keys())[0]):
+>>>>>>> d03f0fbdd (Optimize the matching rule of conditional marks (#14395))
             all_matches.append(condition)
 
     for match in all_matches:
@@ -437,7 +450,11 @@ def find_longest_matches(nodeid, conditions):
         marks = match[case_starting_substring].keys()
         for mark in marks:
             if mark in conditional_marks:
+<<<<<<< HEAD
                 if length >= max_length:
+=======
+                if length > max_length:
+>>>>>>> d03f0fbdd (Optimize the matching rule of conditional marks (#14395))
                     conditional_marks.update({
                         mark: {
                             case_starting_substring: {
@@ -445,6 +462,7 @@ def find_longest_matches(nodeid, conditions):
                         }})
                     max_length = length
             else:
+<<<<<<< HEAD
                 match = None
         else:
             match = nodeid.startswith(condition_entry)
@@ -457,6 +475,21 @@ def find_longest_matches(nodeid, conditions):
             elif length == max_length:
                 longest_matches.append(condition)
     return longest_matches
+=======
+                conditional_marks.update({
+                    mark: {
+                        case_starting_substring: {
+                            mark: match[case_starting_substring][mark]}
+                    }})
+
+    # We may have the same matches of different marks
+    # Need to remove duplicate here
+    for condition in list(conditional_marks.values()):
+        if condition not in matches:
+            matches.append(condition)
+
+    return matches
+>>>>>>> d03f0fbdd (Optimize the matching rule of conditional marks (#14395))
 
 
 def update_issue_status(condition_str, session, basic_facts):
@@ -619,12 +652,12 @@ def pytest_collection_modifyitems(session, config, items):
         json.dumps(basic_facts, indent=2)))
     dynamic_update_skip_reason = session.config.option.dynamic_update_skip_reason
     for item in items:
-        longest_matches = find_longest_matches(item.nodeid, conditions)
+        all_matches = find_all_matches(item.nodeid, conditions)
 
-        if longest_matches:
-            logger.debug('Found match "{}" for test case "{}"'.format(longest_matches, item.nodeid))
+        if all_matches:
+            logger.debug('Found match "{}" for test case "{}"'.format(all_matches, item.nodeid))
 
-            for match in longest_matches:
+            for match in all_matches:
                 # match is a dict which has only one item, so we use match.values()[0] to get its value.
                 for mark_name, mark_details in list(list(match.values())[0].items()):
                     if mark_name == "regex":
