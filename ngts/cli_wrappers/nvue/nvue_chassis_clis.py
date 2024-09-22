007e02808cb029ee8c4e1d2968aadabeed04a0b6
Liran Artzi <lartzi@nvidia.com>
2024-09-22 18:59:12 +0300

    modify TC  algorithm tests due to sonic changes
    Change-Id: I4943f84704adb303e17e98879af0d39d38a510a1

--- ngts/cli_wrappers/nvue/nvue_chassis_clis.py
+++ ngts/cli_wrappers/nvue/nvue_chassis_clis.py
@@ -1,4 +1,5 @@
 from ngts.cli_wrappers.common.chassis_clis_common import ChassisCliCommon
+from ngts.nvos_tools.infra.OutputParsingTool import OutputParsingTool
 
 
 class NvueChassisCli(ChassisCliCommon):
@@ -6,20 +7,30 @@ class NvueChassisCli(ChassisCliCommon):
     This class is for chassis cli commands for NVOS only
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, engine):
+        self.engine = engine
 
-    @staticmethod
-    def show_platform_summary(engine):
+    def show_platform_summary(self):
         """
         This method execute command "show platform summary" on dut
         :param engine: ssh engine object
         :return: the cmd output
         """
-        return engine.run_cmd("show platform summary")
+        return self.engine.run_cmd("cat /etc/sonic/config_db.json")
 
     def get_hostname(self):
         """
         This method is abstractmethod and should be implemented in child classes
         """
         pass
+
+    def parse_platform_summary(self):
+        """
+        Parse the output of "show platform summary"
+        :return: dict, example: {'HwSKU': 'ACS-MSN4410', 'ASIC Count': '1', 'ASIC': 'mellanox'...}
+        """
+        platform_summary = OutputParsingTool.parse_json_str_to_dictionary(
+            self.show_platform_summary()).get_returned_value()["DEVICE_METADATA"]['localhost']
+        platform_summary['Platform'] = platform_summary.pop('platform')
+        platform_summary['HwSKU'] = platform_summary.pop('hwsku')
+        return platform_summary
--- ngts/cli_wrappers/nvue/nvue_cli.py
+++ ngts/cli_wrappers/nvue/nvue_cli.py
@@ -3,17 +3,21 @@ import logging
 from ngts.cli_wrappers.nvue.nvue_chassis_clis import NvueChassisCli
 from ngts.cli_wrappers.sonic.sonic_hw_mgmt_cli import SonicHwMgmtCli
 from ngts.cli_wrappers.nvue.nvue_general_clis import NvueGeneralCli
+from ngts.cli_wrappers.sonic.sonic_interface_clis import SonicInterfaceCli
+from ngts.cli_wrappers.sonic.sonic_im_clis import SonicImClis
 
 logger = logging.getLogger()
 
 
 class NvueCli():
     def __init__(self, topology):
-        self.chassis = NvueChassisCli()
         # self.branch = topology.players['dut'].get('branch')
         self.engine = topology.players['dut']['engine']
+        self.chassis = NvueChassisCli(engine=self.engine)
         self._general = None
         self._hw_mgmt = None
+        self._interface = None
+        self._im = None
 
     @property
     def general(self):
@@ -26,3 +30,15 @@ class NvueCli():
         if self._hw_mgmt is None:
             self._hw_mgmt = SonicHwMgmtCli(engine=self.engine)
         return self._hw_mgmt
+
+    @property
+    def interface(self):
+        if self._interface is None:
+            self._interface = SonicInterfaceCli(engine=self.engine, cli_obj=self)
+        return self._interface
+
+    @property
+    def im(self):
+        if self._im is None:
+            self._im = SonicImClis(engine=self.engine, cli_obj=self)
+        return self._im
