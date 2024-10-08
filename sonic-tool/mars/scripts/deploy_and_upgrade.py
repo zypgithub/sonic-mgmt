b47007526a500e7470d80cfb42532bbb41873ec0
Roman Savchuk <rsavchuk@nvidia.com>
2024-10-08 17:16:46 +0300

    Removed WA for docker ptf as sonic-mgmt issue 13545 closed
    Updated docker ptf tag from 605957 to 669063
    Change-Id: I7f3327773ebaa3ffc58c7951de2484086cb2f30a
    
    Change-Id: Ia4476b8bd737953910af2a90ba49ffc1961e6f79
    (cherry picked from commit 692fb3694698915d98bba971cc687e4f04b4aafd)

--- sonic-tool/mars/scripts/deploy_and_upgrade.py
+++ sonic-tool/mars/scripts/deploy_and_upgrade.py
@@ -402,7 +402,7 @@ def get_ptf_docker_tag(image_path):
             image_path = '/' + '/'.join(image_path.split('/')[file_path_index:])
         branch = get_sonic_branch(image_path)
         logger.info('SONiC branch is: {}'.format(branch))
-        ptf_tag = constants.BRANCH_PTF_MAPPING.get(branch, '605957')
+        ptf_tag = constants.BRANCH_PTF_MAPPING.get(branch, '669063')
     except Exception as err:
         logger.error('Can not get SONiC branch and PTF tag from path: {}, using "latest". Error: {}'.format(image_path,
                                                                                                             err))
