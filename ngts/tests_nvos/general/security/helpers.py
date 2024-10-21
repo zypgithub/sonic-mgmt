24a9558271542f3241bfa96a0727d0fa10b0ce1d
Alon Navarro <alonn@nvidia.com>
2024-10-21 08:47:52 +0300

    [nvos][security] fix host domain mapping command
    suddenly original command doesn't work, so now doing the same in different way
    
    Change-Id: I3ab4c521a6ec82743f462edf51392996d810c7c4

--- ngts/tests_nvos/general/security/helpers.py
+++ ngts/tests_nvos/general/security/helpers.py
@@ -22,4 +22,6 @@ def add_etc_host_mapping_to_dn(dn, address, cmd_runner=None):
 
 def remove_etc_host_mapping_to_dn(dn, cmd_runner=None):
     cmd_runner = cmd_runner or CmdRunner()
-    cmd_runner.run_cmd(f"sudo sed -i '/{dn}/d' {ETC_HOSTS}")
+    # cmd = f"sudo sed -i '/{dn}/d' {ETC_HOSTS}"
+    cmd = f"cp -f {ETC_HOSTS} /tmp/hosts.new && sed -i '/{dn}/d' /tmp/hosts.new && sudo tee {ETC_HOSTS} < /tmp/hosts.new && rm -f /tmp/hosts.new"
+    cmd_runner.run_cmd(cmd)
