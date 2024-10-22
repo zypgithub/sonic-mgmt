7bf927b94dbe4db7e031623f0f88ced341cf21ce
Alon Navarro <alonn@nvidia.com>
2024-10-22 15:11:31 +0300

    [nvos][security] remove scp skip in radius tests
    after bug #4081866 was closed
    
    Change-Id: Iced4ca3c2dc9dcf97e7a22ab4e147ace13ad5bfd

--- ngts/tests_nvos/general/security/radius/conftest.py
+++ ngts/tests_nvos/general/security/radius/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 
 
-# @pytest.fixture(scope='session', autouse=True)
-# def prepare_scp_test(prepare_scp):
-#     return
+@pytest.fixture(scope='session', autouse=True)
+def prepare_scp_test(prepare_scp):
+    return
 
 
 @pytest.fixture(scope='function', autouse=True)
