98ccb7d8be41a1f973a58c79ec27269d1960fc5a
Sviatoslav Demniak <sviatoslavd@mellanox.com>
2024-10-28 04:40:18 +0200

    regression fixed
    Change-Id: I6a2934ef70bdc1f2c6551b01d6b629d477eb4faa

--- ngts/tests_nvos/infra/init_flow/test_system_init_done_feature.py
+++ ngts/tests_nvos/infra/init_flow/test_system_init_done_feature.py
@@ -4,6 +4,7 @@ from ngts.nvos_tools.infra.ConnectionTool import ConnectionTool
 from ngts.nvos_tools.infra.DutUtilsTool import DutUtilsTool
 from ngts.nvos_tools.infra.Tools import Tools
 from ngts.tests_nvos.general.security.conftest import *
+from ngts.tests_nvos.constants import MINUTE
 
 logger = logging.getLogger()
 
@@ -81,7 +82,7 @@ def test_system_ready_state_up(engines, devices, topology_obj):
         assert res_obj.result, res_obj.info
 
 
-@pytest.mark.timeout(20, func_only=True)
+@pytest.mark.timeout(20 * MINUTE, func_only=True)
 @pytest.mark.init_flow
 def test_system_ready_state_down(engines, devices, topology_obj):
     """
