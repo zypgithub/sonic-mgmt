bfa44fb52505b1320da24e1a5be5273d1a7a80ba
Liran Artzi <lartzi@nvidia.com>
2024-10-28 14:55:49 +0200

    system stats fix for juliet
    Change-Id: I6572c43c3d709caa5c03e4a18e1445ec9590aa33

--- ngts/nvos_tools/Devices/IbDevice.py
+++ ngts/nvos_tools/Devices/IbDevice.py
@@ -1090,7 +1090,7 @@ class JulietScaleoutSwitch(JulietSwitch):
         })
 
         self.stats_fan_header_num_of_lines = 21
-        self.stats_cpu_header_num_of_lines = 10
+        self.stats_cpu_header_num_of_lines = 12
         self.stats_temperature_header_num_of_lines = 48
 
         # Port 1-36 is from asic1/ Port 37-72 is from asic2
