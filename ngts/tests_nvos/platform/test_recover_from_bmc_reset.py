34ef78e0fe3f7e5fdc880ee60fb4c79b8b3db20e
Tamuz Ofer <tofer@nvidia.com>
2024-11-03 15:47:16 +0200

    fix health-check in test_recover_from_bmc_reset.py
    Change-Id: Id976e15f53c5de7c98d7c5565268631b2936f87b

--- ngts/tests_nvos/platform/test_recover_from_bmc_reset.py
+++ ngts/tests_nvos/platform/test_recover_from_bmc_reset.py
@@ -1,7 +1,7 @@
 import logging
 from time import sleep
 
-from ngts.nvos_constants.constants_nvos import SystemConsts, PlatformConsts, FansConsts
+from ngts.nvos_constants.constants_nvos import SystemConsts, PlatformConsts, FansConsts, HealthConsts
 from ngts.nvos_tools.infra.BmcTool import BmcTool
 from ngts.nvos_tools.infra.DutUtilsTool import wait_for_specific_regex_in_logs
 from ngts.nvos_tools.infra.OutputParsingTool import OutputParsingTool
@@ -19,10 +19,14 @@ CPU_MAX_UTILIZATION = 25.0
 BMC_LOG_LINES_MAX = 30
 
 
-def test_recover_from_bmc_reset(engines, devices, topology_obj):
+def test_recover_from_bmc_reset(engines, devices, topology_obj, loganalyzer):
     system = System()
     platform = Platform()
     engine = engines.dut
+
+    with allure.step("Add health.err to loganalyzer ignore list"):
+        for log_analyzer_instance in (loganalyzer or {}).values():
+            log_analyzer_instance.ignore_regex.append('health.err')
 
     with allure.step("Assert BMC status is ok before starting test"):
         initial_bmc_status = OutputParsingTool.parse_json_str_to_dictionary(
@@ -30,8 +34,8 @@ def test_recover_from_bmc_reset(engines, devices, topology_obj):
         assert initial_bmc_status[PlatformConsts.INV_STATE] == FansConsts.STATE_OK
 
     with allure.step("Get health issues before starting test"):
-        initial_health_issues = OutputParsingTool.parse_json_str_to_dictionary(system.health.show()
-                                                                               ).get_returned_value().keys()
+        initial_health_issues = OutputParsingTool.parse_json_str_to_dictionary(
+            system.health.show()).get_returned_value()[HealthConsts.ISSUES].keys()
 
     with allure.step("Reset BMC"):
         BmcTool.reset(engine)
@@ -39,35 +43,35 @@ def test_recover_from_bmc_reset(engines, devices, topology_obj):
     try:
         with allure.step("Wait for log line indicating BMC is back on"):
             wait_for_specific_regex_in_logs(engine, BMC_ON_LOG_LINE, LOG_DELAY)
-            logger.info("waiting 60 seconds...")
-            sleep(60)
+            logger.info("waiting 2:30 minutes for system to recover...")
+            sleep(150)
 
         with allure.step("Assert BMC status is ok"):
             final_bmc_status = OutputParsingTool.parse_json_str_to_dictionary(
                 platform.inventory.show('BMC')).get_returned_value()
             assert final_bmc_status == initial_bmc_status
 
+        with allure.step("Check general system status"):
+            with allure.independent_step("Assert health ok"):
+                final_health_issues = OutputParsingTool.parse_json_str_to_dictionary(
+                    system.health.show()).get_returned_value()[HealthConsts.ISSUES].keys()
+                assert set(final_health_issues) <= set(initial_health_issues)
+
+            with allure.independent_step("Assert show-firmware command is not broken"):
+                firmware_output = platform.firmware.show("BMC")
+                assert "actual-firmware" in firmware_output
+                # test_show_platform_firmware(engines, devices, test_api, output_format)
+
+            with allure.independent_step("Assert CPU usage"):
+                cpu_output = OutputParsingTool.parse_json_str_to_dictionary(system.show("cpu")).get_returned_value()
+                cpu_utilization = cpu_output[SystemConsts.CPU_UTILIZATION_KEY]
+                assert cpu_utilization <= CPU_MAX_UTILIZATION
+
+            with allure.independent_step("Assert logs aren't flooded with BMC error messages"):
+                bmc_log_lines = system.log.show(op_param=" | grep -ie bmc").splitlines()
+                assert len(bmc_log_lines) < BMC_LOG_LINES_MAX
+
     except Exception:
-        with allure.step("BMC failed to reset or failed to recover. Fixing by remote-reboot."):
+        with allure.step("Failed to recover from BMC reset. Fixing device by remote-reboot."):
             recover_dut_with_remote_reboot(topology_obj, engines, should_clear_config=False)
         raise
-
-    with allure.step("Check general system status"):
-        with allure.independent_step("Assert health ok"):
-            final_health_issues = OutputParsingTool.parse_json_str_to_dictionary(system.health.show()
-                                                                                 ).get_returned_value().keys()
-            assert set(final_health_issues) <= set(initial_health_issues)
-
-        with allure.independent_step("Assert show-firmware command is not broken"):
-            firmware_output = platform.firmware.show("BMC")
-            assert "actual-firmware" in firmware_output
-            # test_show_platform_firmware(engines, devices, test_api, output_format)
-
-        with allure.independent_step("Assert CPU usage"):
-            cpu_output = OutputParsingTool.parse_json_str_to_dictionary(system.show("cpu")).get_returned_value()
-            cpu_utilization = cpu_output[SystemConsts.CPU_UTILIZATION_KEY]
-            assert cpu_utilization <= CPU_MAX_UTILIZATION
-
-        with allure.independent_step("Assert logs aren't flooded with BMC error messages"):
-            bmc_log_lines = system.log.show(op_param=" | grep -ie bmc").splitlines()
-            assert len(bmc_log_lines) < BMC_LOG_LINES_MAX
