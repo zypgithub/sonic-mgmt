873ea6c841e48b8e0877eea20d0060eead398f4d
Venkata Ghantasala <vghantasala@mtl-vdi-986.wap.labs.mlnx>
2024-11-07 23:54:27 +0200

    Add SNMP local host testcases in test_platform_cumulus_only.py. Some testcases commented out for future use
    Change-Id: I98391eaac2c13e6a93f257b3f3e55c6cdc73bcae

--- ngts/nvos_tools/infra/HostMethods.py
+++ ngts/nvos_tools/infra/HostMethods.py
@@ -20,7 +20,20 @@ class HostMethods:
     @staticmethod
     def host_snmp_walk(host_engine, ip_address, community='qwerty12', mib='', param=''):
         with allure_step("Running snmpwalk command"):
-            return host_engine.run_cmd('snmpwalk -v 1 -c {0} {1} {2}'.format(community, ip_address, param))
+            return host_engine.run_cmd('snmpwalk -v 1 -c {0} {1} {2}'.format(community, ip_address, mib + param))
+
+    @staticmethod
+    def host_snmp_walk_v2(host_engine, ip_address, community='defaultuser', mib='', param='', port=None):
+        with allure_step("Running snmpwalk v2 command"):
+            if not port:
+                return host_engine.run_cmd(f'snmpwalk -v2c -c {community} {ip_address} {mib + param}')
+            else:
+                return host_engine.run_cmd(f'snmpwalk -v2c -c {community} {ip_address}:{port} {mib + param}')
+
+    @staticmethod
+    def host_snmp_getnext(host_engine, ip_address, community='defaultuser', mib='', param=''):
+        with allure_step("Running snmpgetnext command"):
+            return host_engine.run_cmd('snmpgetnext -v2c -c {0} {1} {2}'.format(community, ip_address, mib + param))
 
     @staticmethod
     def host_ip_address_set(host_engine, ip_address, interface):
@@ -38,12 +51,26 @@ class HostMethods:
             return host_engine.run_cmd('ping -I {0} {1} -c {2}'.format(interface, ip_address, count))
 
     @staticmethod
-    def start_snmp_server(engine, state, readonly_community, listening_address):
+    def start_snmp_server(engine, state, readonly_community, listening_address, access="", vrf="", port=None, cumulus=False):
         system = System(None)
         system.snmp_server.set('state', state).verify_result()
         system.snmp_server.set('readonly-community', readonly_community).verify_result()
+        if access:
+            system.snmp_server.readonly_community.set(f'{readonly_community} access', access).verify_result()
         system.snmp_server.set('listening-address', listening_address).verify_result()
-        NvueGeneralCli.apply_config(engine)
+        if vrf:
+            system.snmp_server.listening_address.set(f'{listening_address} vrf', vrf).verify_result()
+        if port:
+            system.snmp_server.listening_address.set(f'{listening_address} port', port).verify_result()
+        if cumulus:
+            logging.info("Adding Cumulus MIBs")
+            system.snmp_server.set('mibs', "cumulus-sensor-mib").verify_result()
+            system.snmp_server.set('mibs', "cumulus-status-mib").verify_result()
+            engine.run_cmd('sudo sed -i \'s/#mibs +CUMULUS-STATUS-MIB/mibs +CUMULUS-STATUS-MIB/g\' /etc/snmp/snmp.conf')
+            engine.run_cmd('sudo sed -i \'s/#mibs +CUMULUS-SENSOR-MIB/mibs +CUMULUS-SENSOR-MIB/g\' /etc/snmp/snmp.conf')
+            engine.run_cmd("sudo systemctl reset-failed")
+            engine.run_cmd("sudo systemctl restart snmpd.service")
+        NvueGeneralCli.apply_config(engine, ask_for_confirmation=True)
         logging.info("Snmp enabled successfully")
 
     @staticmethod
