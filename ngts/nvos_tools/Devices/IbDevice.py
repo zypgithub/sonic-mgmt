ed624a7876fa1593f71d1ce07ce6c22d1983acd5
Elias Abboud <eabboud@nvidia.com>
2024-11-12 09:32:19 +0200

    Adjusted expected value for addition-info field
    Change-Id: I851ca0c53e98993db2fc66f26591b62e8521d8da

--- ngts/nvos_tools/Devices/IbDevice.py
+++ ngts/nvos_tools/Devices/IbDevice.py
@@ -1031,8 +1031,8 @@ class JulietScaleoutSwitch(JulietSwitch):
     def _init_constants(self):
         super()._init_constants()
         self.asic_type = NvosConst.NVL5
-        self.cluster_app_nmx_controller = {'addition-info': None, 'app-id': 'nmx-c-nvos', 'app-ver': None, 'capabilities': 'sm, gfm, fib, gw-api', 'components-ver': None, 'reason': '', 'status': 'ok'}
-        self.cluster_app_nmx_telemetry = {'addition-info': None, 'app-id': 'nmx-telemetry', 'app-ver': None, 'capabilities': 'ib-telemetry', 'components-ver': None, 'reason': '', 'status': 'ok'}
+        self.cluster_app_nmx_controller = {'addition-info': ExpectedString(regex=".*"), 'app-id': 'nmx-c-nvos', 'app-ver': None, 'capabilities': 'sm, gfm, fib, gw-api', 'components-ver': None, 'reason': '', 'status': 'ok'}
+        self.cluster_app_nmx_telemetry = {'addition-info': ExpectedString(regex=".*"), 'app-id': 'nmx-telemetry', 'app-ver': None, 'capabilities': 'ib-telemetry', 'components-ver': None, 'reason': '', 'status': 'ok'}
         self.cluster_app = {
             'nmx-controller': {key: value for key, value in self.cluster_app_nmx_controller.items() if key not in ['reason', 'status']},
             'nmx-telemetry': {key: value for key, value in self.cluster_app_nmx_telemetry.items() if key not in ['reason', 'status']}
