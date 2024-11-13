a7d520905fb17e67dd3668f4bcb98943633b9769
Illia Kotvitskyi <ikotvitskyi@nvidia.com>
2024-11-13 10:14:21 +0200

    update secrets regexp
      * remove redundant repetitive pipe
    
      * make secret name required
    
    Change-Id: I04c9034902a37347316aeac71bd47131d94be828

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -151,7 +151,7 @@ def create_secrets_vars_script(conn, mars_docker_env_secrets, container_name):
     export_env_var_script_path = "/tmp/{CONTAINER_NAME}_export_env_var.sh".format(CONTAINER_NAME=container_name)
     if os.path.exists(export_env_var_script_path):
         conn.run("rm -f {SCRIPT_PATH}".format(SCRIPT_PATH=export_env_var_script_path), warn=True)
-    regex = r"[\w|_]*=[\'|\w|\d|$|!|-|\.|\/|\-|:|~|\(|\)|@]*"
+    regex = r"[\w|_]+=[|\'\w\d$!-\.\/\-:~\(\)@=]*"
     env_vars = re.findall(regex, mars_docker_env_secrets)
     script_content = ["export {0}".format(env_var.replace("$", r"\$")) for env_var in env_vars]
     script_content = ["#!/bin/bash"] + script_content
