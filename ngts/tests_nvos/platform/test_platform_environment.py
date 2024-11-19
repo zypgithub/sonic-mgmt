2ad2069303e60937fd4fa271bede80d039eb0091
Tamuz Ofer <tofer@nvidia.com>
2024-11-19 16:07:19 +0200

    increased delay for detecting fan error
    Change-Id: I749fac77ffadef9f8d85525c8502426f21d7055f

--- ngts/tests_nvos/platform/test_platform_environment.py
+++ ngts/tests_nvos/platform/test_platform_environment.py
@@ -606,7 +606,7 @@ def fan_error_context(fan: str, device, engine):
             cmd_output = engine.run_cmd(f"sudo ln -sf {tmp_file.path} {fan_fault_file}")
             if cmd_output:
                 raise Exception("Test failure. See command and output above.")
-            time.sleep(5)
+            time.sleep(10)
             try:
                 fan_output = platform.environment.fan.show(fan)
                 assert (OutputParsingTool.parse_json_str_to_dictionary(fan_output).get_returned_value()
@@ -617,7 +617,7 @@ def fan_error_context(fan: str, device, engine):
                 cmd_output = engine.run_cmd(f"sudo ln -sf {fan_fault_original_target} {fan_fault_file}")
                 if cmd_output:
                     raise Exception("Test failure. See command and output above.")
-                time.sleep(5)
+                time.sleep(10)
                 fan_output = platform.environment.fan.show(fan)
                 assert (OutputParsingTool.parse_json_str_to_dictionary(fan_output).get_returned_value()
                         [PlatformConsts.ENV_TEMP_STATE_PROP] == PlatformConsts.ENV_TEMP_STATE_OK), \
