14926d11fee0b9dbd84095a54ac9bef024921dc9
Liran Artzi <lartzi@nvidia.com>
2024-11-27 12:35:12 +0200

    increase system is ready timeout
    Change-Id: Ie6de56f761f5ff80227fc15055f1dc3e78ccb166

--- ngts/nvos_tools/Devices/IbDevice.py
+++ ngts/nvos_tools/Devices/IbDevice.py
@@ -509,6 +509,7 @@ class GorillaSwitchBF3(GorillaSwitch):
 
     def _init_constants(self):
         super()._init_constants()
+        self.system_is_ready_wait_timeout = 10 * MINUTE
         self.constants.firmware.remove(PlatformConsts.FW_BIOS)
         self.ib_ports_num = 64
         self.core_count = 16
@@ -531,7 +532,7 @@ class BlackMambaSwitch(IbSwitch):
     def _init_constants(self):
         self.asic_amount = 4
         super()._init_constants()
-        self.system_is_ready_wait_timeout = 5 * MINUTE
+        self.system_is_ready_wait_timeout = 15 * MINUTE
         self.ib_ports_num = 2 * 72
         self.core_count = 4
         self.asic_type = NvosConst.QTM3
@@ -638,7 +639,7 @@ class CrocodileSwitch(IbSwitch):
         self.core_count = 4
         self.split_ports_supported = True
         self.asic_type = NvosConst.QTM3
-        self.system_is_ready_wait_timeout = 5 * MINUTE
+        self.system_is_ready_wait_timeout = 10 * MINUTE
         self.platform_file_path = MultiPlanarConsts.PLATFORM_FILE_FULL_PATH.format("x86_64-nvidia_qm3400-r0")
         self.show_platform_output.update({
             "product-name": "QM3400",
