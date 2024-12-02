b78bb9e81ce22b5b287cdc7837933375c14fd95d
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-12-02 09:03:42 +0800

    [Bugfix] Fix missing imports for platform_api_conn in tests/platform_tests/api (#15807)
    What is the motivation for this PR?
    In PR #15605, the platform_api_conn fixture was relocated to a common location. However, some imports were overlooked in the scripts within the tests/platform_tests/api folder, leading to test failures. This PR addresses the issue by adding the missing imports to resolve the errors.
    
    How did you do it?
    This PR addresses the issue by adding the missing imports to resolve the errors.
    
    Change-Id: I1fd1ec4dcf564ac1b847d7d765d25e4913fc66b3

--- tests/platform_tests/api/test_sfp.py
+++ tests/platform_tests/api/test_sfp.py
@@ -15,6 +15,7 @@ from tests.common.platform.transceiver_utils import is_sw_control_enabled,\
     get_port_expected_error_state_for_mellanox_device_on_sw_control_enabled
 from tests.common.mellanox_data import is_mellanox_device
 from collections import defaultdict
+from tests.common.platform.device_utils import platform_api_conn    # noqa F401
 
 from .platform_api_test_base import PlatformApiTestBase
 
