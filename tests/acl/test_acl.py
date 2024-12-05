d49c3e56b8964773403d640dff2128e5f7617a73
Yutong Zhang <90831468+yutongzhang-microsoft@users.noreply.github.com>
2024-12-05 14:04:21 +0800

    Refactor function get_iface_ip to common location (#15893)
    What is the motivation for this PR?
    In PR #15619, a cross-feature dependency was introduced in tests/acl/test_acl.py due to the usage of the get_iface_ip function. To address this issue and streamline the code structure, we have refactored the function and moved it to a common location.
    
    How did you do it?
    To address this issue and streamline the code structure, we have refactored the function and moved it to a common location.
    
    How did you verify/test it?
    
    Change-Id: I5f33fb99d63ccfd1142df15eeab5cf1e1593414f

--- tests/acl/test_acl.py
+++ tests/acl/test_acl.py
@@ -26,7 +26,7 @@ from tests.common.utilities import get_upstream_neigh_type, get_downstream_neigh
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts # noqa F401
 from tests.common.platform.processes_utils import wait_critical_processes
 from tests.common.platform.interface_utils import check_all_interface_information
-from tests.qos.tunnel_qos_remap_base import get_iface_ip
+from tests.common.utilities import get_iface_ip
 
 logger = logging.getLogger(__name__)
 
