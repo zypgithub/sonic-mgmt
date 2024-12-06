d160307c80a1505d880d0e0927e4b5ed416b3980
Cong Hou <congh@nvidia.com>
2024-12-06 03:48:07 +0200

    Revert "Add new tests for crm DRAM on switch"
    This reverts commit 694f7ea6272d2075feb0f6f13c5ca23ed23cf3eb.
    
    Reason for revert: The feature has been reverted, so should be the test.
    
    Change-Id: I798fd6d80637f9e907346c60699c05c48354cf84

--- tests/common/helpers/crm.py
+++ tests/common/helpers/crm.py
@@ -8,17 +8,17 @@ EXPECT_CLEAR = ".* THRESHOLD_CLEAR .*"
 
 THR_VERIFY_CMDS = OrderedDict([
     ("exceeded_used", "bash -c \"crm config thresholds {{crm_cli_res}}  type used; \
-         crm config thresholds {{crm_cli_res}} low {{crm_used|int - 1 - dram_margin|int * 2}}; \
-         crm config thresholds {{crm_cli_res}} high {{crm_used|int - dram_margin|int}}\""),
+         crm config thresholds {{crm_cli_res}} low {{crm_used|int - 1}}; \
+         crm config thresholds {{crm_cli_res}} high {{crm_used|int}}\""),
     ("clear_used", "bash -c \"crm config thresholds {{crm_cli_res}} type used && \
-         crm config thresholds {{crm_cli_res}} low {{crm_used|int + dram_margin|int}} && \
-         crm config thresholds {{crm_cli_res}} high {{crm_used|int + 1 + dram_margin|int * 2}}\""),
+         crm config thresholds {{crm_cli_res}} low {{crm_used|int}} && \
+         crm config thresholds {{crm_cli_res}} high {{crm_used|int + 1}}\""),
     ("exceeded_free", "bash -c \"crm config thresholds {{crm_cli_res}} type free && \
-         crm config thresholds {{crm_cli_res}} low {{crm_avail|int - 1 - dram_margin|int * 2}} && \
-         crm config thresholds {{crm_cli_res}} high {{crm_avail|int - dram_margin|int}}\""),
+         crm config thresholds {{crm_cli_res}} low {{crm_avail|int - 1}} && \
+         crm config thresholds {{crm_cli_res}} high {{crm_avail|int}}\""),
     ("clear_free", "bash -c \"crm config thresholds {{crm_cli_res}} type free && \
-         crm config thresholds {{crm_cli_res}} low {{crm_avail|int + dram_margin|int}} && \
-         crm config thresholds {{crm_cli_res}} high {{crm_avail|int + 1 + dram_margin|int * 2}}\""),
+         crm config thresholds {{crm_cli_res}} low {{crm_avail|int}} && \
+         crm config thresholds {{crm_cli_res}} high {{crm_avail|int + 1}}\""),
     ("exceeded_percentage", "bash -c \"crm config thresholds {{crm_cli_res}} type percentage && \
          crm config thresholds {{crm_cli_res}} low {{th_lo|int}} && \
          crm config thresholds {{crm_cli_res}} high {{th_hi|int}}\""),
