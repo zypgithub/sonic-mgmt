a42232aa9f4282ed845aca0e7d3491b628377ebf
rowaida mahameed <rmahameed@nvidia.com>
2024-12-09 07:30:11 +0200

    NVOS [transceiver firmware upgrade] - test update
    Change-Id: Id532257351078bbd88820bbfe37c5209fbecf1a4

---
+++ ngts/tests_nvos/platform/constants.py
@@ -0,0 +1,70 @@
+class Transceiver:
+    def __init__(self, transceiver_type, last_release_path, test_versions_path,
+                 upgrade_version_name, downgrade_version_name,
+                 upgrade_version_number, downgrade_version_number,
+                 installation_time):
+        self.transceiver_type = transceiver_type
+        self.last_release_path = last_release_path
+        self.test_versions_path = test_versions_path
+        self.upgrade_version_name = upgrade_version_name
+        self.downgrade_version_name = downgrade_version_name
+        self.upgrade_version_number = upgrade_version_number
+        self.downgrade_version_number = downgrade_version_number
+        self.installation_time = installation_time
+
+
+class TransceiversConsts:
+    TRANSCEIVER_TYPE = 'type'
+    TRANSCEIVER_LAST_RELEASE_PATH = "releases_path"
+    TRANSCEIVER_TEST_VERSIONS_PATH = "test_versions_path"
+    TRANSCEIVER_UPGRADE_VERSION_NAME = 'upgrade_versions'
+    TRANSCEIVER_DOWNGRADE_VERSION_NAME = 'downgrade_versions'
+    TRANSCEIVER_UPGRADE_VERSION_NUMBER = 'upgrade_fw'
+    TRANSCEIVER_DOWNGRADE_VERSION_NUMBER = 'downgrade_fw'
+    TRANSCEIVER_INSTALLATION_TIME = "installation_time"
+    TRANSCEIVERS_APPROVED_FIRMWARES_PATH = '/auto/sw_system_project/NVOS_INFRA/verification_files/transceiver_fw/'
+    TRANSCEIVERS_FIRMWARES_PATH = '/.autodirect/sw/release/fwshared/linkx/mlnx_linkx_module_aoc_fw/'
+    TRANSCEIVERS_RELEASE = '230_00_release'     # the latest approved release
+
+    TRANSCEIVERS_DETAILS = {
+        '39': Transceiver(
+            transceiver_type='Xodin',
+            last_release_path=f"{TRANSCEIVERS_FIRMWARES_PATH}39_Xodin/{TRANSCEIVERS_RELEASE}",
+            test_versions_path=f"{TRANSCEIVERS_APPROVED_FIRMWARES_PATH}Xodin/",
+            upgrade_version_name="fw_39_230_00019_dev_signed.bin",
+            downgrade_version_name="fw_39_230_00018_dev_signed.bin",
+            upgrade_version_number="39.230.19",
+            downgrade_version_number="39.230.18",
+            installation_time=90
+        ),
+        '70': Transceiver(
+            transceiver_type='Wolverine',
+            last_release_path=f"{TRANSCEIVERS_FIRMWARES_PATH}70_Wolverine/{TRANSCEIVERS_RELEASE}",
+            test_versions_path=f"{TRANSCEIVERS_APPROVED_FIRMWARES_PATH}Wolverine/",
+            upgrade_version_name="fw_70_230_00013_dev_signed_WOLVERINE_DK.bin",
+            downgrade_version_name="fw_70_220_01076_dev_signed_WOLVERINE_DK.bin",
+            upgrade_version_number="70.230.013",
+            downgrade_version_number="70.220.1076",
+            installation_time=200
+        ),
+        '46': Transceiver(
+            transceiver_type='Bagheera',
+            last_release_path=f"{TRANSCEIVERS_FIRMWARES_PATH}46_Bagheera1_2/{TRANSCEIVERS_RELEASE}",
+            test_versions_path=f"{TRANSCEIVERS_APPROVED_FIRMWARES_PATH}Bagheera/",
+            upgrade_version_name="",
+            downgrade_version_name="",
+            upgrade_version_number="",
+            downgrade_version_number="",
+            installation_time=90
+        ),
+        '47': Transceiver(
+            transceiver_type='Louie',
+            last_release_path=f"{TRANSCEIVERS_FIRMWARES_PATH}47_Louie1_4/{TRANSCEIVERS_RELEASE}",
+            test_versions_path=f"{TRANSCEIVERS_APPROVED_FIRMWARES_PATH}Louie/",
+            upgrade_version_name="",
+            downgrade_version_name="",
+            upgrade_version_number="",
+            downgrade_version_number="",
+            installation_time=90
+        )
+    }
