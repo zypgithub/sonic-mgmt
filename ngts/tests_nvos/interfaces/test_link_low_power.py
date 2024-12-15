8925ce1bc4392586f9500feb616216a20d6dd20c
rowaida mahameed <rmahameed@nvidia.com>
2024-12-15 22:16:13 +0200

    NVOS [L1 - saving power] - disabled the feature as a default value
    Change-Id: Id583337d81c7636947a0ca3a9f8e17a65d502ec4

--- ngts/tests_nvos/interfaces/test_link_low_power.py
+++ ngts/tests_nvos/interfaces/test_link_low_power.py
@@ -1,6 +1,5 @@
 import logging
 import time
-
 import pytest
 
 from ngts.nvos_constants.constants_nvos import ApiType, NvosConst
@@ -16,7 +15,7 @@ logger = logging.getLogger()
 
 @pytest.mark.interface
 @pytest.mark.parametrize('test_api', ApiType.ALL_TYPES)
-def test_link_low_power_enabled(engines, devices, prepare_traffic, test_api):
+def test_link_low_power_disabled(engines, devices, prepare_traffic, test_api):
     """
     validate feature default values using ports and show command
 
@@ -25,12 +24,12 @@ def test_link_low_power_enabled(engines, devices, prepare_traffic, test_api):
 
     Test flow:
     1. Run nv show ib link-low-power
-    2. validate feature enabled
+    2. validate feature disabled
     3. validate for all ports l1_cap = 1
     """
     TestToolkit.tested_api = test_api
 
-    _test_l1_behavior(engine=engines.dut, device=devices.dut, expected_state=NvosConst.ENABLED, expected_l1_cap_value=1)
+    _test_l1_behavior(engine=engines.dut, device=devices.dut, expected_state=NvosConst.DISABLED, expected_l1_cap_value=0)
 
     # TODO - once we can send traffic we need to add case sending traffic and validate
     #  no time difference between sending with feature enabled or disabled.
@@ -38,36 +37,36 @@ def test_link_low_power_enabled(engines, devices, prepare_traffic, test_api):
 
 @pytest.mark.interface
 @pytest.mark.parametrize('test_api', ApiType.ALL_TYPES)
-def test_link_low_power_disabled(engines, devices, topology_obj, prepare_traffic, test_api):
+def test_link_low_power_enabled(engines, devices, topology_obj, prepare_traffic, test_api):
     """
-    testing disabling the feature and verify show command and ports
+    testing enabling the feature and verify show command and ports
 
     Test flow:
-    1. Run nv set fae ib link-low-power state disabled + apply
+    1. Run nv set fae ib link-low-power state enabled + apply
     2. Run nv show ib link-low-power
-    3. validate feature disabled
-    4. validate for all ports l1_cap = 0
+    3. validate feature enabled
+    4. validate for all ports l1_cap = 1
     5. Run nv unset fae ib link-low-power state + apply
     6. Run nv show ib link-low-power
-    7. validate feature enabled
-    8. validate for all ports l1_cap = 1
+    7. validate feature disabled
+    8. validate for all ports l1_cap = 0
     """
     TestToolkit.tested_api = test_api
     fae = Fae()
 
     try:
         with allure.step("disabled l1 saving power capability"):
-            fae.ib.link_low_power.set(op_param_name=IbInterfaceConsts.LINK_STATE, op_param_value=NvosConst.DISABLED, apply=True)
+            fae.ib.link_low_power.set(op_param_name=IbInterfaceConsts.LINK_STATE, op_param_value=NvosConst.ENABLED, apply=True)
             with allure.step("wait until ports update"):
                 time.sleep(50)
 
-        _test_l1_behavior(engine=engines.dut, device=devices.dut, expected_state=NvosConst.DISABLED, expected_l1_cap_value=0)
+            _test_l1_behavior(engine=engines.dut, device=devices.dut, expected_state=NvosConst.ENABLED, expected_l1_cap_value=1)
 
     finally:
         fae.ib.link_low_power.unset(IbInterfaceConsts.LINK_STATE, apply=True)
         with allure.step("wait until ports update"):
             time.sleep(50)
-        _test_l1_behavior(engine=engines.dut, device=devices.dut, expected_state=NvosConst.ENABLED, expected_l1_cap_value=1)
+        _test_l1_behavior(engine=engines.dut, device=devices.dut, expected_state=NvosConst.DISABLED, expected_l1_cap_value=0)
 
 
 def validate_all_ports_l1_capability(expected_status, engine, device):
