864602937915371cb6093b8b857f0e571f4752f7
Cong Hou <97947969+congh-nvidia@users.noreply.github.com>
2024-12-19 14:56:22 +0800

    Update the conditional mark skip logic to support regex (#15977)
    - What is the motivation for this PR?
    Update the conditional mark skip logic to support regular expression
    
    - How did you do it?
    Please see the summary
    
    - How did you verify/test it?
    Run regression with this change and skipped conditions with the regex key, no issues observed.
    
    Change-Id: I5506e4552b9f41e6aeeb34a0fc3a0499f92635b9

--- tests/common/plugins/conditional_mark/__init__.py
+++ tests/common/plugins/conditional_mark/__init__.py
@@ -421,7 +421,18 @@ def find_all_matches(nodeid, conditions):
 
     for condition in conditions:
         # condition is a dict which has only one item, so we use condition.keys()[0] to get its key.
-        if nodeid.startswith(list(condition.keys())[0]):
+        condition_entry = list(condition.keys())[0]
+        condition_items = condition[condition_entry]
+        if "regex" in condition_items.keys():
+            assert isinstance(condition_items["regex"], bool), \
+                "The value of 'regex' in the mark conditions yaml should be bool type."
+            if condition_items["regex"] is True:
+                match = re.search(condition_entry, nodeid)
+            else:
+                match = None
+        else:
+            match = nodeid.startswith(condition_entry)
+        if match:
             all_matches.append(condition)
 
     for match in all_matches:
