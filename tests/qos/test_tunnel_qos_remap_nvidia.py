dc194e3f8b2da468e4dc96f65f755fbb5f5ade2c
echuawu <chuanw@nvidia.com>
2024-12-23 17:34:31 +0800

    Change dscp remap case support from dualtor AS to AA topology
    Change dscp remap case support from dualtor AS to AA topology
    
    Change-Id: I73bd16d448e68e37efa4f90cebeec150df017624

--- tests/qos/test_tunnel_qos_remap_nvidia.py
+++ tests/qos/test_tunnel_qos_remap_nvidia.py
@@ -3,11 +3,11 @@ import pytest
 import random
 import ptf.packet as scapy
 from tests.common.helpers.assertions import pytest_assert
-from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports_to_upper_tor  # noqa F401
-from tests.common.dualtor.mux_simulator_control import toggle_simulator_port_to_lower_tor  # noqa F401
-from tests.common.dualtor.dual_tor_utils import upper_tor_host, lower_tor_host, dualtor_info  # noqa F401
+from tests.common.dualtor.nic_simulator_control import toggle_ports
+from tests.common.dualtor.dual_tor_utils import dualtor_info  # noqa F401
 from tests.common.fixtures.ptfhost_utils import change_mac_addresses, run_icmp_responder, run_garp_service  # noqa F401
-from tests.common.dualtor.mux_simulator_control import toggle_all_simulator_ports_to_rand_selected_tor # noqa F401
+from tests.common.dualtor.dual_tor_utils import toggle_all_aa_ports_to_rand_selected_tor, \
+    toggle_all_aa_ports_to_rand_unselected_tor  # noqa F401
 from ptf.mask import Mask
 from ptf import testutils
 from tests.common.utilities import wait_until
@@ -25,20 +25,20 @@ PKT_NUM = 100
 
 
 @pytest.fixture(scope='module', autouse=True)
-def config_counter_poll_interval(upper_tor_host, lower_tor_host):  # noqa F811
+def config_counter_poll_interval(rand_selected_dut, rand_unselected_dut):  # noqa F811
     """
     Fixture to set the counterpoll intervals to 1s during the test, so we can save some time.
     """
-    origin_queue_interval = upper_tor_host.get_counter_poll_status()['QUEUE_STAT']['interval']
-    upper_tor_host.set_counter_poll_interval('QUEUE_STAT', 1000)
-    lower_tor_host.set_counter_poll_interval('QUEUE_STAT', 1000)
+    origin_queue_interval = rand_selected_dut.get_counter_poll_status()['QUEUE_STAT']['interval']
+    rand_selected_dut.set_counter_poll_interval('QUEUE_STAT', 1000)
+    rand_unselected_dut.set_counter_poll_interval('QUEUE_STAT', 1000)
     yield
-    upper_tor_host.set_counter_poll_interval('QUEUE_STAT', origin_queue_interval)
-    lower_tor_host.set_counter_poll_interval('QUEUE_STAT', origin_queue_interval)
+    rand_selected_dut.set_counter_poll_interval('QUEUE_STAT', origin_queue_interval)
+    rand_unselected_dut.set_counter_poll_interval('QUEUE_STAT', origin_queue_interval)
 
 
-def test_active_tor_normal_traffic(ptfhost, rand_selected_dut, rand_unselected_dut, upper_tor_host, # noqa F811
-                                   lower_tor_host, toggle_all_simulator_ports_to_rand_selected_tor, # noqa F811
+def test_active_tor_normal_traffic(ptfhost, rand_selected_dut, rand_unselected_dut, # noqa F811
+                                   toggle_all_aa_ports_to_rand_selected_tor, # noqa F811
                                    tbinfo, ptfadapter):
     """
     The test is to verify the dscp of normal traffic(not bounced back traffic) on active tor is not remapped, and the
@@ -69,7 +69,7 @@ def test_active_tor_normal_traffic(ptfhost, rand_selected_dut, rand_unselected_d
         (63, 1)
     }
     with allure.step("Prepare test parameters"):
-        dualtor_meta = dualtor_info(ptfhost, upper_tor_host, lower_tor_host, tbinfo)
+        dualtor_meta = dualtor_info(ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo)
         mg_facts = rand_selected_dut.get_extended_minigraph_facts(tbinfo)
         active_tor_mac = rand_selected_dut.facts['router_mac']
         uplink_interfaces = {}
@@ -139,9 +139,9 @@ def test_active_tor_normal_traffic(ptfhost, rand_selected_dut, rand_unselected_d
                 "Please check the output of cmd 'show queue counters' in the test log.".format(dscp, queue, PKT_NUM))
 
 
-def test_traffic_between_servers(ptfhost, upper_tor_host, lower_tor_host, # noqa F811
-                                 toggle_all_simulator_ports_to_upper_tor, # noqa F811
-                                 toggle_simulator_port_to_lower_tor, tbinfo, ptfadapter): # noqa F811
+def test_traffic_between_servers(ptfhost, rand_selected_dut, rand_unselected_dut,  # noqa F811
+                                 toggle_all_aa_ports_to_rand_selected_tor,  # noqa F811
+                                 tbinfo, ptfadapter): # noqa F811
     """
     The test case is to verify on a same tor, the traffic from an active server to a standby server should be
     forwarded by the tunnel, and enter the correct queue.
@@ -158,15 +158,15 @@ def test_traffic_between_servers(ptfhost, upper_tor_host, lower_tor_host, # noqa
         (48, 48, 7)
     ]
     with allure.step("Prepare test parameters and switch one server on the lower tor to active"):
-        dualtor_meta = dualtor_info(ptfhost, upper_tor_host, lower_tor_host, tbinfo)
-        lower_tor_mg_facts = lower_tor_host.get_extended_minigraph_facts(tbinfo)
-        upper_tor_mg_facts = upper_tor_host.get_extended_minigraph_facts(tbinfo)
+        dualtor_meta = dualtor_info(ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo)
+        lower_tor_mg_facts = rand_unselected_dut.get_extended_minigraph_facts(tbinfo)
+        upper_tor_mg_facts = rand_selected_dut.get_extended_minigraph_facts(tbinfo)
         vlan_interface = list(lower_tor_mg_facts['minigraph_vlans'].keys())[0]
         # use the last interface as the active interface on lower tor
         lower_tor_active_interface = lower_tor_mg_facts['minigraph_vlans'][vlan_interface]['members'][-1]
         lower_tor_active_server_ptf_port = lower_tor_mg_facts['minigraph_ptf_indices'][lower_tor_active_interface]
         logger.info("Toggle the interface {} to active on the lower tor.".format(lower_tor_active_interface))
-        toggle_simulator_port_to_lower_tor(lower_tor_active_interface)
+        toggle_ports(rand_unselected_dut, [lower_tor_active_interface], "active")
         lower_tor_uplink_interfaces = {}
         t1_ptf_ports = []
         for portchannel in lower_tor_mg_facts['minigraph_portchannels'].values():
@@ -180,10 +180,10 @@ def test_traffic_between_servers(ptfhost, upper_tor_host, lower_tor_host, # noqa
         lower_tor_standby_server = lower_tor_mg_facts['minigraph_neighbors'][lower_tor_standby_interface]['name']
         lower_tor_standby_server_ip = lower_tor_mg_facts['minigraph_devices'][lower_tor_standby_server]['lo_addr']
 
-        upper_tor_host_name = upper_tor_mg_facts['inventory_hostname']
-        lower_tor_host_name = lower_tor_mg_facts['inventory_hostname']
-        upper_tor_loopback0_ip = upper_tor_mg_facts['minigraph_devices'][upper_tor_host_name]['lo_addr']
-        lower_tor_loopback0_ip = lower_tor_mg_facts['minigraph_devices'][lower_tor_host_name]['lo_addr']
+        rand_selected_dut_name = upper_tor_mg_facts['inventory_hostname']
+        rand_unselected_dut_name = lower_tor_mg_facts['inventory_hostname']
+        upper_tor_loopback0_ip = upper_tor_mg_facts['minigraph_devices'][rand_selected_dut_name]['lo_addr']
+        lower_tor_loopback0_ip = lower_tor_mg_facts['minigraph_devices'][rand_unselected_dut_name]['lo_addr']
 
     with allure.step("Start the traffic test"):
         for dscp, outer_dscp, queue in dscp_to_queue_mapping:
@@ -198,8 +198,8 @@ def test_traffic_between_servers(ptfhost, upper_tor_host, lower_tor_host, # noqa
             # The ttl of inner_frame is decreased by 1
             pkt.ttl -= 1
             ipinip_packet = simple_ipv4ip_packet(
-                eth_dst=upper_tor_host.facts['router_mac'],
-                eth_src=lower_tor_host.facts['router_mac'],
+                eth_dst=rand_selected_dut.facts['router_mac'],
+                eth_src=rand_unselected_dut.facts['router_mac'],
                 ip_src=lower_tor_loopback0_ip,
                 ip_dst=upper_tor_loopback0_ip,
                 ip_dscp=outer_dscp,
@@ -217,14 +217,14 @@ def test_traffic_between_servers(ptfhost, upper_tor_host, lower_tor_host, # noqa
                 scapy.IP, "flags")  # don't fragment flag will be set on NVidia platforms
 
             # Clear queue counters before sending traffic
-            lower_tor_host.shell('sonic-clear queuecounters')
+            rand_unselected_dut.shell('sonic-clear queuecounters')
             ptfadapter.dataplane.flush()
             testutils.send(ptfadapter, lower_tor_active_server_ptf_port, pkt, PKT_NUM)
             receive_port_index, _ = testutils.verify_packet_any_port(ptfadapter, exp_tunnel_pkt, t1_ptf_ports)
             receive_ptf_port = t1_ptf_ports[receive_port_index]
             # Verify queue counters in all possible interfaces
             pytest_assert(wait_until(
-                10, 2, 0, check_queue_counter, lower_tor_host,
+                10, 2, 0, check_queue_counter, rand_unselected_dut,
                 [lower_tor_uplink_interfaces[receive_ptf_port]], queue, PKT_NUM),
                 "The queue counter for DSCP: {}, Queue: {} is not as expected: >= {}. "
                 "Please check the output of cmd 'show queue counters' in the test log.".format(dscp, queue, PKT_NUM))
