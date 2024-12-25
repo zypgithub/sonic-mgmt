61afe2b1d5dbb0e4028e50b63e093f5cb45e74c1
Asaf Lavi <asafl@nvidia.com>
2024-12-25 10:22:30 +0200

    fix for handle component Issue: NA Signed-off-by: Asaf Lavi <asafl@nvidia.com> Change-Id: I4b74ea88c43a3e64b7a55e7930ed28dd8ef1f1d3
    Change-Id: I1646290d219321891b975e855e8ab19ae7c0e469
    (cherry picked from commit ecd9fd24cae7526fa4f7745b0a595bf13bc643dd)

--- build/ci/handle_component.groovy
+++ build/ci/handle_component.groovy
@@ -110,7 +110,7 @@ def run_step(name) {
             if (env.CHECK_YAML_ONLY == "true"){
                 print "The changes are only in the error/skip yaml files, run only the yaml validation test on SPC SIMX."
                 // Only run one SPC SIMX setup
-                set_bat_skip(name, SONIC_CANONICAL_BAT + SONIC_CANONICAL_DPU_BAT + NVOS_BAT - "SPC_SIMX")
+                set_bat_skip(name, SONIC_CANONICAL_BAT + NVOS_BAT - "SPC_SIMX")
                 env.SIMX_NONE_BLOCKER = "false"
             }
         }
