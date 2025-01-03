7068b0f8d881f0da11e8aedce4fbab8c77c9146f
Wei Guo <weguo@nvidia.com>
2025-01-03 16:39:44 +0800

    Add hwsku Mellanox-SN2700-A1-C28D8 with T0 for r-panther-01
    Change-Id: Ic5dfa3f3b91f4cb2b425b2d6e7e08bab137ba0b1

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -178,6 +178,19 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
             for i in s100G_ports:
                 alias = "etp%d" % (i / 4 + 1)
                 port_alias_to_name_map[alias] = "Ethernet%d" % i
+        elif hwsku in ["Mellanox-SN2700-C28D8", "Mellanox-SN2700-A1-C28D8"]:
+            # 50G ports
+            s50G_ports = [x for x in range(112, 127, 2)]
+
+            # 100G ports
+            s100G_ports = [x for x in range(0, 109, 4)]
+
+            for i in s50G_ports:
+                alias = "etp%d" % (i / 4 + 1) + ("a" if i % 4 == 0 else "b")
+                port_alias_to_name_map[alias] = "Ethernet%d" % i
+            for i in s100G_ports:
+                alias = "etp%d" % (i / 4 + 1)
+                port_alias_to_name_map[alias] = "Ethernet%d" % i
         elif hwsku == "Mellanox-SN3800-D112C8":
             x_ports = [x for x in range(0, 95, 2)]
             for i in x_ports:
