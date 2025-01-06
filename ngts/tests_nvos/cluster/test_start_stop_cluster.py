c77177415d4dd535269a1cf07a7e004a1e521143
Elias Abboud <eabboud@nvidia.com>
2025-01-06 12:48:47 +0200

    Adjusted output of nv show cluster apps installed
    Change-Id: I429daf21cae5ef6a93e20f9c86c1ddb571f5ec76

--- ngts/tests_nvos/cluster/test_start_stop_cluster.py
+++ ngts/tests_nvos/cluster/test_start_stop_cluster.py
@@ -83,7 +83,7 @@ def test_cluster_app_start_stop(engines, devices, test_api, has_loopbox, standal
                 cluster.apps.installed.show(output_format=output_format),
                 output_format=output_format).get_returned_value()
             for app in INITIAL_EXPECTED_APPS:
-                ValidationTool.validate_output_of_show(output[app], devices.dut.cluster_app[app]).verify_result()
+                ValidationTool.validate_output_of_show(output[app], devices.dut.cluster_app_installed[app]).verify_result()
 
         with allure.step("Running 'nv show cluster apps running' command and verifying output"):
             ClusterTools.wait_for_apps_to_be_in_wanted_state()
