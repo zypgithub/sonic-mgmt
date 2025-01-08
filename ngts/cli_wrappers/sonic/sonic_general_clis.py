241ca8b14c2080ee1f3c9161bcc501eafc92c651
Roman Savchuk <rsavchuk@nvidia.com>
2025-01-08 09:02:08 +0200

    Remove qos apply for bison simx issue link: https://redmine.mellanox.com/issues/4156055
    Change-Id: I4d0aa0b0515d7c2e4ad974c0eb23bb3b32060d37

--- ngts/cli_wrappers/sonic/sonic_general_clis.py
+++ ngts/cli_wrappers/sonic/sonic_general_clis.py
@@ -855,7 +855,7 @@ class SonicGeneralCliDefault(GeneralCliCommon):
                 self.reboot_reload_flow(r_type=SonicConst.CONFIG_RELOAD_CMD, topology_obj=topology_obj,
                                         reload_force=True)
 
-        if not self.is_performance_setup(setup_name):
+        if not self.is_performance_setup(setup_name) and 'bison-simx' not in setup_name:
             with allure.step("Apply qos and dynamic buffer config"):
                 self.cli_obj.qos.reload_qos()
                 self.verify_dockers_are_up(dockers_list=['swss'])
