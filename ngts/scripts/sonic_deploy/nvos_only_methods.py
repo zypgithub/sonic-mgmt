368d583d8b0231d275ff208e6967c1abceeeb25e
Alon Navarro <alonn@nvidia.com>
2025-01-08 19:05:31 +0200

    [NV] fix cleanup step after upgrade in deploy script
    Change-Id: I9f8d816624061712a54191b433ecb473417e22d2

--- ngts/scripts/sonic_deploy/nvos_only_methods.py
+++ ngts/scripts/sonic_deploy/nvos_only_methods.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import shutil
+import time
 
 import yaml
 
@@ -23,7 +24,6 @@ from ngts.tests_nvos.general.post_upgrade_switch.constants import UPGRADE_STATUS
     UPGRADE_STATUS_FILE_PATH, InstallSteps
 from ngts.tests_nvos.general.post_upgrade_switch.install_steps_timer import InstallStepsTimer
 from ngts.tests_nvos.general.security.bmc.bmc_creds.constants import BmcUsers
-from ngts.tests_nvos.helpers.redmine_helpers import is_bug_active
 from ngts.tools.test_utils import allure_utils as allure
 
 logger = logging.getLogger()
@@ -148,14 +148,11 @@ class NvosInstallationSteps:
         with allure.step('Clear fetched files for the tests'):
             system = System()
             dut_engine.disconnect()  # force engines.dut to reconnect
-            if is_bug_active(4132303):
-                with allure.step('work around bug #4132303 - let first connection to get stuck and force extra connection'):
-                    # TODO: remove once bug #4132303 is closed
-                    try:
-                        dut_engine.run_cmd('echo "tmp cmd"', timeout=10)
-                    except Exception:
-                        dut_engine.disconnect()
-                    dut_engine.disconnect()
+            # in the cleanup we unset interface which sets back default ACL rules (to mgmt ports)
+            # in this case, ongoing sessions may get stuck/interrupted. thus, wait few seconds after that configuration
+            # more info in redmine: #4132303
+            logging.info('sleep after applied default ACL rules in cleanup')
+            time.sleep(10)
 
             with allure.step('Delete fetched image file'):
                 system.image.files.delete_all_existing_files(engine=dut_engine)
