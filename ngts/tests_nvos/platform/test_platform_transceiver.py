fe502c9a286e2df62d8f93af2cce2d8f1f920dd9
Aleksander Romashin <aromashin@nvidia.com>
2025-01-16 13:04:32 +0200

    [nvos] Add detail to PMPE test, fix lldp
    Change-Id: I1e9635d5f513b572dd191eb051a21aceb964f6d5

--- ngts/tests_nvos/platform/test_platform_transceiver.py
+++ ngts/tests_nvos/platform/test_platform_transceiver.py
@@ -202,9 +202,12 @@ def _verify_link_state_down(down_ports):
 
 def _get_module_with_status(platform, status):
     with allure.step(f"Find {status} module"):
+        detail = ""
+        if TestToolkit.tested_api == ApiType.NVUE:
+            detail = "detail"
         transceivers = [name for name, transceiver in
                         OutputParsingTool.parse_json_str_to_dictionary(
-                            platform.transceiver.show()).get_returned_value().items() if
+                            platform.transceiver.show(detail)).get_returned_value().items() if
                         "sw" in name and transceiver[PlatformConsts.TRANSCEIVER_STATUS] == status]
         if not transceivers:
             pytest.skip(f"No {status} transceivers found for setup")
