de21e0b63c6cd25c57a2de2c410d80e1123d2cc7
Liran Artzi <lartzi@nvidia.com>
2025-02-05 10:51:16 +0200

    update opensm path for multiplanar systems
    Change-Id: Ie99f58748f60d5949eed5b85b3ae7db81d5f3805

--- ngts/nvos_tools/ib/opensm/OpenSmTool.py
+++ ngts/nvos_tools/ib/opensm/OpenSmTool.py
@@ -51,9 +51,9 @@ class OpenSmTool:
                 output = engines.hfnm.run_cmd("ibdev2netdev")
                 if "smi2" not in output:
                     engines.hfnm.run_cmd(f"/opt/mellanox/iproute2/sbin/rdma dev add smi2 type SMI parent {port_name}")
-                    OPEN_SM_PATH = '/opt/ufm/opensm/sbin/opensm'
+                opensm_path = '/opt/ufm/opensm/sbin/opensm'
             else:
-                OPEN_SM_PATH = '/labhome/juliav/workspace/sm_regression/sources/SM_MASTER/usr/sbin/opensm'
+                opensm_path = '/labhome/juliav/workspace/sm_regression/sources/SM_MASTER/usr/sbin/opensm'
             output = engines.hfnm.run_cmd("ibstat {}".format(port_name))
             guid = ''
             for line in output.splitlines():
@@ -66,7 +66,7 @@ class OpenSmTool:
 
         with (allure.step("Start OpenSM")):
             # todo: remove when we get opensm 5.22 or later
-            engines.hfnm.run_cmd(f"{OPEN_SM_PATH} -F {OPEN_SM_CFG_PATH} -g {guid} -B")
+            engines.hfnm.run_cmd(f"{opensm_path} -F {OPEN_SM_CFG_PATH} -g {guid} -B")
             time.sleep(5)
 
         with allure.step("Verify OpenSM is running"):
