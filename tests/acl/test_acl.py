0509dd769f0b1dc0c4f258d77844f6e429b5d003
Nana He <nhe@nvidia.com>
2025-02-07 16:28:34 +0800

    Align test case for new t0 topo
    Align acl test for t0-isolated-d16u16s1
    
    Change-Id: Idf5c73cf84506be799c5ea5542168995ec04b3e0

--- tests/acl/test_acl.py
+++ tests/acl/test_acl.py
@@ -381,9 +381,13 @@ def setup(duthosts, ptfhost, rand_selected_dut, rand_unselected_dut, tbinfo, ptf
             if namespace:
                 acl_table_ports[''] += port
 
-    if topo in ["t0", "m0_vlan", "m0_l3"] or tbinfo["topo"]["name"] in ("t1-lag", "t1-64-lag", "t1-64-lag-clet",
-                                                                        "t1-56-lag", "t1-28-lag", "t1-32-lag",
-                                                                        "t1-lag-c224o8"):
+    if len(port_channels) and topo in ["t0", "m0_vlan", "m0_l3"] or tbinfo["topo"]["name"] in ("t1-lag", "t1-64-lag",
+                                                                                               "t1-64-lag-clet",
+                                                                                               "t1-56-lag",
+                                                                                               "t1-28-lag",
+                                                                                               "t1-32-lag",
+                                                                                               "t1-lag-c224o8"):
+
         for k, v in list(port_channels.items()):
             acl_table_ports[v['namespace']].append(k)
             # In multi-asic we need config both in host and namespace.
