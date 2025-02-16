a51dca89902f8a97196935a0bdaa2b0f6dd9baaf
Tamuz Ofer <tofer@nvidia.com>
2025-02-16 18:01:11 +0200

    Revert "update to bug-handler with string-similarity"
    This reverts commit de869a06da44320bfc07cbb3955865e2345b7250.
    
    Reason for revert: New bug-handler version has trouble matching against older bugs
    
    Change-Id: If97147686503f407d2ee2db61490a48594f0c6f1

--- ngts/constants/constants.py
+++ ngts/constants/constants.py
@@ -1566,7 +1566,7 @@ class BugHandlerConst:
     BUG_HANDLER_CONF_FILE = {"SONiC-Design": os.path.join(NGTS_PATH, "helpers/bug_handler/sonic_bug_handler.conf"),
                              "'NVOS - Design'": os.path.join(NGTS_PATH, "helpers/bug_handler/nvos_bug_handler.conf")}
     BUG_HANDLER_PYTHON_PATH = "/mswg/projects/swvt/MARS/scripts/python37_wrapper.sh"
-    BUG_HANDLER_PATH = "/auto/sw_tools/Internal/BugHandling/RELEASES/1_4_1/bin/"
+    BUG_HANDLER_PATH = "/auto/sw_tools/Internal/BugHandling/RELEASES/1_4_1_BETA/bin/"
     BUG_HANDLER_SCRIPT = BUG_HANDLER_PATH + "handle_bug.py"
     BUG_HANDLER_UPLOAD_ATTACHMENT_SCRIPT = BUG_HANDLER_PATH + "upload_attachment_to_bug.py"
     BUG_HANDLER_SANITIZER_USER = "asan"
