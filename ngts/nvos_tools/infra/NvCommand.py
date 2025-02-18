74195219a6c0b824ac125984bd35058471367c50
Liran Artzi <lartzi@nvidia.com>
2025-02-18 11:53:34 +0200

    add fae to NvCommand class
    Change-Id: I21fb9279b932997099e7a8453f7de1f74906b19b

--- ngts/nvos_tools/infra/NvCommand.py
+++ ngts/nvos_tools/infra/NvCommand.py
@@ -4,6 +4,7 @@ from ngts.nvos_tools.acl.acl import Acl
 from ngts.nvos_tools.ib.Ib import Ib
 from ngts.nvos_tools.ib.InterfaceConfiguration.Port import Port
 from ngts.nvos_tools.infra.DefaultDict import DefaultDict
+from ngts.nvos_tools.infra.Fae import Fae
 from ngts.nvos_tools.platform.Platform import Platform
 from ngts.nvos_tools.system.System import System
 from typing import Dict
@@ -12,7 +13,8 @@ from typing import Dict
 class NvCommand:
     def __init__(self):
         self.acl = Acl()
+        self.fae = Fae()
         self.ib = Ib()
         self.platform = Platform()
-        self.system = System()
         self.port: Dict[str, Port] = DefaultDict(lambda port: Port(name=port))
+        self.system = System()
