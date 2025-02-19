c039a5cde276044a0db848b114954f8c76eab422
Alon Navarro <alonn@nvidia.com>
2025-02-19 09:35:24 +0200

    [nv] add option to prevent ssh with engine before OpenApi operation
    Change-Id: Ibd46c0adb202b537dfb47b517935951331160926

--- ngts/cli_wrappers/openapi/openapi_base_clis.py
+++ ngts/cli_wrappers/openapi/openapi_base_clis.py
@@ -12,22 +12,28 @@ class OpenApiBaseCli:
     cli_name = ""
 
     @staticmethod
-    def show(engine, resource_path, op_param="", output_format=OutputFormat.json):
+    def show(engine, resource_path, op_param="", output_format=OutputFormat.json, check_engine_connectivity: bool = True):
         logging.info("Running GET method on dut using openApi for {}".format(resource_path))
-        return OpenApiCommandHelper.execute_script(engine.engine.username, engine.engine.password,
+        if check_engine_connectivity:
+            engine = engine.engine
+        return OpenApiCommandHelper.execute_script(engine.username, engine.password,
                                                    OpenApiReqType.GET, engine.ip, resource_path, op_param)
 
     @staticmethod
-    def set(engine, resource_path, op_param_name="", op_param_value=""):
+    def set(engine, resource_path, op_param_name="", op_param_value="", check_engine_connectivity: bool = True):
         logging.info("Running PATCH method on dut using openApi for {}".format(resource_path))
-        return OpenApiCommandHelper.execute_script(engine.engine.username, engine.engine.password,
+        if check_engine_connectivity:
+            engine = engine.engine
+        return OpenApiCommandHelper.execute_script(engine.username, engine.password,
                                                    OpenApiReqType.PATCH, engine.ip, resource_path, op_param_name,
                                                    op_param_value)
 
     @staticmethod
-    def unset(engine, resource_path, op_param=""):
+    def unset(engine, resource_path, op_param="", check_engine_connectivity: bool = True):
         logging.info("Running DELETE method on dut using openApi for {}".format(resource_path))
-        return OpenApiCommandHelper.execute_script(engine.engine.username, engine.engine.password,
+        if check_engine_connectivity:
+            engine = engine.engine
+        return OpenApiCommandHelper.execute_script(engine.username, engine.password,
                                                    OpenApiReqType.DELETE, engine.ip, resource_path, op_param, None)
 
     @staticmethod
