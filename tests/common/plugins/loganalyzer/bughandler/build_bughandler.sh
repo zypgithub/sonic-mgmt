6a708a7fb1bdae1039a295532685a7aabbc410b9
Xixue Jia <xixuej@nvidia.com>
2025-03-20 12:49:32 +0800

    Enhance build_bughandler.sh with cleanup function for recovery of init.py files
    Change-Id: I2263351eefde6a0fba5748a291fbcd433ef2df0f

--- tests/common/plugins/loganalyzer/bughandler/build_bughandler.sh
+++ tests/common/plugins/loganalyzer/bughandler/build_bughandler.sh
@@ -13,15 +13,20 @@ echo "" > ../../../__init__.py
 echo "Clean up the content in tests/common/plugins/loganalyzer/__init__.py file to avoid the loading other unneeded module"
 echo "" > ../__init__.py
 
+cleanup() {
+    echo "Recover the tests/common/__init__.py"
+    mv ../../../__init__.py.1 ../../../__init__.py
+    echo "Recover the tests/common/plugins/loganalyzer/__init__.py"
+    mv ../__init__.py.1 ../__init__.py
+}
+
+trap cleanup EXIT
+
+
 $BUG_HANDLER_VENV/pip install -r requirements.txt
-$BUG_HANDLER_VENV/pyinstaller bug_handler.spec --noconfirm --clean
+$BUG_HANDLER_VENV/pyinstaller bug_handler.spec --noconfirm --clean 
 
 #cp the ./dist/bug_handler/bug_handler and ./dist/bug_handler/_internal to /auto/sw_system_release/sonic/bughandler
-echo "Recover the tests/common/__init__.py"
-mv ../../../__init__.py.1 ../../../__init__.py
-echo "Recover the tests/common/plugins/loganalyzer/__init__.py"
-mv ../__init__.py.1 ../__init__.py
-
 echo "Copy the bughandler standalone tool to /auto/sw_system_release/sonic/bughandler"
 cp ./dist/bug_handler/bug_handler /auto/sw_system_release/sonic/bughandler
 cp -r ./dist/bug_handler/_internal /auto/sw_system_release/sonic/bughandler
