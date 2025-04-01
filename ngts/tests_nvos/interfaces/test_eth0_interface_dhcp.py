c09f80f0b8842e24170328c85f3a1601da04cb73
Sviatoslav Demniak <sviatoslavd@mellanox.com>
2025-04-01 17:02:19 +0300

    added fix for eth0 dhcp hostname test
    Change-Id: I8091bcd5c1ae995c9b861a85c25973d3963db21b

--- ngts/tests_nvos/interfaces/test_eth0_interface_dhcp.py
+++ ngts/tests_nvos/interfaces/test_eth0_interface_dhcp.py
@@ -655,7 +655,7 @@ def wait_for_mtu_changed(port_obj, mtu_to_verify):
         assert current_mtu == mtu_to_verify, "Current mtu {} is not as expected {}".format(current_mtu, mtu_to_verify)
 
 
-@retry(Exception, tries=10, delay=2)
+@retry(Exception, tries=15, delay=2)
 def wait_for_hostname_changed(system, dhcp_hostname):
     with (allure.step("Waiting for system hostname changed to {}".format(dhcp_hostname))):
         system_output = OutputParsingTool.parse_json_str_to_dictionary(system.show()).get_returned_value()
