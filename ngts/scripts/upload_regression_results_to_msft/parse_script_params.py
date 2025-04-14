3862227c0e545a432ad3e3b0ca39e4bd85eb80e9
Aharon Malkin <amalkin@nvidia.com>
2025-04-14 12:28:05 +0300

    Added Bison to the upload to MSFT script
    Change-Id: I438710040ac3c6e458968375f1e46eea72a94ac0

--- ngts/scripts/upload_regression_results_to_msft/parse_script_params.py
+++ ngts/scripts/upload_regression_results_to_msft/parse_script_params.py
@@ -26,11 +26,11 @@ def init_parser():
 
 def get_platform_filter_param(params):
     platform_options = ["2010", "2100", "2410", "2700", "3420", "3700", "3800",
-                        "4280", "4410", "4600", "4600C", "4700", "5400", "5600", "5610"]
+                        "4280", "4410", "4600", "4600C", "4700", "5400", "5600", "5610", "5640"]
     start_idx = 1 if params[0] == "," else 0
     end_idx = -1 if params[-1] == "," else None
     updated_params = params[start_idx:end_idx] if end_idx is not None else params[start_idx:]
-    platform_filter_param = updated_params.split(',')[-16:]
+    platform_filter_param = updated_params.split(',')[-len(platform_options) - 1:]
     if platform_filter_param[0] == "false":
         new_params = params.replace(",".join(platform_filter_param), "false,")
     else:
