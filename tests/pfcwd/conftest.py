a4e26df5094e827143dc81f56a7955bacf1e5360
Nana He <nhe@nvidia.com>
2025-04-21 19:12:29 +0800

    Fix the issue introduced by the merge
    Change-Id: Ic06d28bb0b717d4be705f6ba892cef800702a7ad

--- tests/pfcwd/conftest.py
+++ tests/pfcwd/conftest.py
@@ -319,7 +319,7 @@ def set_pfc_timer_cisco_8000(duthost, asic_id, script, port):
     duthost.shell(f"show platform npu script {asic_arg} -s {script_name}")
 
 
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture(autouse=True, scope="module")
 def cleanup(duthosts, ptfhost, enum_rand_one_per_hwsku_frontend_hostname):
     """
     Fixture that remove ip address of ethX interface at ptf and clear arp at dut
