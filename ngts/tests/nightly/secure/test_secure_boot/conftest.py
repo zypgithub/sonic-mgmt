d59a0378192c1bcae1f14c05f1ed517af7772731
Nana He <nhe@nvidia.com>
2025-04-22 14:50:49 +0800

    Fix the test issue in secure boot
    Change-Id: I413af7736636055733fc3bdbc6ba45ef50dad6a1
    (cherry picked from commit f3771e5325da8269281fc52f94d1ce7bf00bc55e)

--- ngts/tests/nightly/secure/test_secure_boot/conftest.py
+++ ngts/tests/nightly/secure/test_secure_boot/conftest.py
@@ -22,10 +22,11 @@ def post_secure_boot_steps(secure_boot_helper, topology_obj, setup_name, platfor
 
     yield
 
+    dut = setup_info['duts'][0]
     secure_boot_helper.restore_basic_config(topology_obj, setup_name, platform_params, is_air)
     SonicInstallationSteps.post_installation_steps(
         topology_obj=topology_obj, sonic_topo='ptf-any', recover_by_reboot=True, setup_name=setup_name,
         platform_params=platform_params, apply_base_config=True, target_version="",
         is_shutdown_bgp=False, reboot_after_install=False, deploy_only_target=False, fw_pkg_path="",
-        reboot="no", additional_apps="", setup_info=setup_info, chip_type=chip_type,
+        reboot="no", additional_apps="", setup_info=setup_info, dut_alias=dut['dut_alias'], chip_type=chip_type,
         is_performance=is_performance, xml_rpc=False, is_air=is_air)
