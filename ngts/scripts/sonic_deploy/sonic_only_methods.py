986a8e3bdffb2e109900767e96326a2aaa24f68e
echuawu <chuanw@nvidia.com>
2025-04-25 09:59:41 +0800

    Bring up r-bison-16_setup
    Bring up r-bison-16_setup
    
    Change-Id: I5992b394f7b65a2a03b22362eadc69fcc3532b51
    (cherry picked from commit c25c6952da17db5240f233f9c4a2e3f55293de53)

--- ngts/scripts/sonic_deploy/sonic_only_methods.py
+++ ngts/scripts/sonic_deploy/sonic_only_methods.py
@@ -96,7 +96,7 @@ class SonicInstallationSteps:
                 "mtvr-hippo-02" != dut_name and 'bobcat' not in dut_name and "r-moose-01" != dut_name and
                 "mtvr-moose-04" != dut_name and "r-leopard-01" != dut_name and "r-leopard-58" != dut_name and
                 'r-tigon-04' != dut_name and "mtvr-moose-13" != dut_name and "mtvr-moose-14" != dut_name and
-                "mtvr-gaur-02" != dut_name and "mtvr-gaur-03" != dut_name):
+                "mtvr-gaur-02" != dut_name and "mtvr-gaur-03" != dut_name and "r-bison-16" != dut_name):
             gen_mg_cmd = get_generate_minigraph_cmd(setup_info, dut_name, sonic_topo, port_number)
             run_background_process_on_host(threads_dict, 'generate_minigraph', gen_mg_cmd, timeout=300,
                                            exec_path=ansible_path)
@@ -482,6 +482,9 @@ class SonicInstallationSteps:
             need_gen_mingraph = True
         if "mtvr-gaur-02" in setup_name or "mtvr-gaur-03" in setup_name:
             hwskus = ['Mellanox-SN5610N-C256S2', 'Mellanox-SN5610N-C224O8']
+            need_gen_mingraph = True
+        if "r-bison-16" in setup_name:
+            hwskus = ['Mellanox-SN5640-C512S2', 'Mellanox-SN5640-C448O16']
             need_gen_mingraph = True
 
         for hwsku in hwskus:
