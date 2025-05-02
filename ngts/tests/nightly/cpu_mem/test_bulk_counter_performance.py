44dd8a56a9651183133e92d9e5af9cb4e0371163
Roman Savchuk <rsavchuk@nvidia.com>
2025-05-02 12:20:36 +0300

    Added x86_64-nvidia_sn5640_simx-r0 to bulk counters test
    Change-Id: Ia3fc30a577f47310786f7020993d3824d625c441

--- ngts/tests/nightly/cpu_mem/test_bulk_counter_performance.py
+++ ngts/tests/nightly/cpu_mem/test_bulk_counter_performance.py
@@ -21,6 +21,7 @@ AMPLIFY_FACTORY_FOR_SIMX_MEMORY_THR = {
     "x86_64-nvidia_sn4280_simx-r0": 1.25,
     "x86_64-mlnx_msn4700_simx-r0": 1.25,
     "x86_64-nvidia_sn5600_simx-r0": 1.25,
+    "x86_64-nvidia_sn5640_simx-r0": 1.25
 }
 
 
