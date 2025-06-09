076160f6e6a1164946662b745b7b97f8ee44b1d1
itkoren <itkoren@nvidia.com>
2025-06-09 14:02:27 +0300

    [NVOS] Fix clear history command and replace patch test
    Change-Id: I038e16b471f772e88e663232cfe9cb1f22ea6876

--- ngts/tests_nvos/general/config_commands/test_replace_patch.py
+++ ngts/tests_nvos/general/config_commands/test_replace_patch.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 import pytest
 
@@ -93,8 +94,8 @@ def test_replace_positive(engines, devices):
                                                                             '"password": "{passw}"'.format(passw=devices.dut.default_password))
         file = create_file_with_content(engines.dut, 'replace', 'yaml', config_after_hostname_change)
         output = TestToolkit.GeneralApi[TestToolkit.tested_api].replace_config(engines.dut, file)
-        expected_message = "Loading config file: replace.yaml from current directory."
-        assert expected_message in output, "Failed to replace config"
+        expected_pattern = r"created \[rev_id: \d+\]"
+        assert re.search(expected_pattern, output), f"Failed to replace config - output '{output}' does not match expected pattern '{expected_pattern}'"
 
     with allure.step("Delete created yaml file: {}".format(file)):
         engines.dut.run_cmd('sudo rm {file}'.format(file=file))
