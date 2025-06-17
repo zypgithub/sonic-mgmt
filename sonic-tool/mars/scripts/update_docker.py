6f6493a9efdf8e83d5642b5faa729748af3290c3
Yana Port <yport@nvidia.com>
2025-06-17 17:51:45 +0300

    Devts tag updated to 1.2.430
    Change-Id: I26fdafd31dc225a09c5c29a9abdef7c56ae4be1e

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -400,7 +400,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.427'}
+    default_list = {'docker-ngts': '1.2.430'}
     return default_list.get(docker_name, latest)
 
 
