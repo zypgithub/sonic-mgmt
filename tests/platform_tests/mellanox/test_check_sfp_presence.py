ab7d5e2052fda97b6092c6bd9ad9b764d1cc3d9d
v-jessgeorge <v-jessgeorge@microsoft.com>
2025-06-24 19:10:34 +0530

    Pre-commit errors fixed and Replaced noqa with noqa: in tests/platform_tests 26 files (#19063)
    Pre-commit Changes made in tests/platform_tests which has 26 files.
    
    Noqa replaced with noqa:
    Pre-commit errors like Spacing issues have been fixed
    
    Change-Id: Ieb6733ee8a61d2c9be5d8fe83b352202202cff90

--- tests/platform_tests/mellanox/test_check_sfp_presence.py
+++ tests/platform_tests/mellanox/test_check_sfp_presence.py
@@ -5,7 +5,7 @@ import logging
 import json
 import pytest
 
-from tests.common.fixtures.conn_graph_facts import conn_graph_facts     # noqa F401
+from tests.common.fixtures.conn_graph_facts import conn_graph_facts     # noqa: F401
 
 pytestmark = [
     pytest.mark.asic('mellanox', 'nvidia-bluefield'),
@@ -30,12 +30,12 @@ def parse_sfp_presence_output(sfp_presence_output):
     return sfp_presence_parsed
 
 
-def test_check_sfp_presence(duthosts, rand_one_dut_hostname, conn_graph_facts, dpu_npu_port_list):     # noqa F811
+def test_check_sfp_presence(duthosts, rand_one_dut_hostname, conn_graph_facts, dpu_npu_port_list):     # noqa: F811
     """This test case is to check SFP presence status with CLI and sysfs.
     """
     duthost = duthosts[rand_one_dut_hostname]
-    ports_config = json.loads(duthost.command("sudo sonic-cfggen -d --var-json PORT")["stdout"])    # noqa F841
-    check_intf_presence_command = 'show interface transceiver presence'
+    ports_config = json.loads(duthost.command("sudo sonic-cfggen -d --var-json PORT")["stdout"])    # noqa: F841
+    check_intf_presence_command = 'show interface transceiver presence {}'
 
     logging.info("Use show interface status information")
     check_presence_output = duthost.command(check_intf_presence_command)
