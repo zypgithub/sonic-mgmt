e17c47e297aa405ecbe0d20b549f209216557467
Nana He <nhe@nvidia.com>
2025-07-23 13:00:29 +0800

    Do not run the teardown for memory check when the test case is skipped In one pytest session, when all test case are skipped, then the teardown will not be executed, when there is some test case not skipped, then for the skipped test case, it will still run the teardown.
    Change-Id: Id601823c94166e1abac57a4a1f2f73e2c0278370
    (cherry picked from commit 60a6edc7da6428e328ccbcb578cf6735afb789e1)

--- tests/common/plugins/memory_utilization/__init__.py
+++ tests/common/plugins/memory_utilization/__init__.py
@@ -66,7 +66,7 @@ def pytest_runtest_setup(item):
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_runtest_teardown(item, nextitem):
-    if item.rep_setup.skipped:
+    if item.rep_setup.skipped or item.rep_call.skipped:
         return
     logger.debug("pytest_runtest_teardown early for test {}".format(item.name))
     if "request" in item.fixturenames:
