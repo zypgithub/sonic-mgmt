406122b86bfc905bfff0535a04db452fd5567d55
Ori Trabelsi <otrabelsi@nvidia.com>
2025-08-04 16:41:22 +0300

    Improve test_null_route_helper by replacing sleep with a wait-until mechanism.
    Replace time sleep with a wait-until approach to ensure the rule is fully inserted into the ACL table before testing it.
    
    Change-Id: Ida7f337ebace675c6ab345260018f344fc4dd97a
    (cherry picked from commit 7eff3cd4ba74e3c105361fe3709092361a32b019)
    
    Fix test_null_route_helper
    
    Add import to wait_until.
    Check if rule was added and activate.
    
    Change-Id: I209999f0778ee27d46afddb2c7f10d9fb298a504

--- tests/acl/null_route/test_null_route_helper.py
+++ tests/acl/null_route/test_null_route_helper.py
@@ -16,6 +16,7 @@ from tests.common.plugins.loganalyzer.loganalyzer import LogAnalyzer, LogAnalyze
 from tests.common.utilities import get_upstream_neigh_type
 from tests.common.utilities import get_neighbor_ptf_port_list
 from tests.common.utilities import get_neighbor_port_list
+from tests.common.utilities import wait_until
 
 logger = logging.getLogger(__name__)
 
@@ -250,18 +251,24 @@ def verify_test_data_rule_inserted_to_acl_table(duthost, action):
     ip = action.split()[-1]
     rule_type = action.split()[0].upper()
     find_rule_in_table = duthost.shell(f'show acl rule {action.split()[1]} | grep -E {ip}', module_ignore_errors=True)['stdout_lines']
-    logger.info(f"Search result for {action} in ACL table: {find_rule_in_table}")
+    logger.info(f"Verifying action: {action} was applied in ACL table")
     if not find_rule_in_table and rule_type == "UNBLOCK":
+        logger.info(f"[UNBLOCK] Expected: No rule for {ip} | Actual: No rule found (traffic allowed by default)")
         return True
 
     if find_rule_in_table and rule_type == "BLOCK":
+        logger.info(f"[BLOCK] Expected: Rule for {ip} to be found and active | Actual: {find_rule_in_table}")
         for rule in find_rule_in_table:
-            rule_from_table = rule.split()[1]
-            is_rule_added = ip in rule_from_table and rule_type in rule_from_table
+            rule_split = rule.split()
+            if len(rule_split) < 2:
+                continue
+            rule_from_table = rule_split[1]
+            is_rule_active = rule_split[-1] == "Active"
+            is_rule_added = ip in rule_from_table and rule_type in rule_from_table and is_rule_active
             if is_rule_added:
                 return True
 
-    logger.info(f"Rule {action} not found in ACL table")
+    logger.info(f"Rule {action} was not applied in ACL table")
     acl_table = duthost.shell(f'show acl rule {action.split()[1]}', module_ignore_errors=True)['stdout']
     logger.info(f"ACL table: {acl_table}")
     return False
