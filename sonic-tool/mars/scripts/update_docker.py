25b9ab51b260925c6755ce8e32b58dd7bf45e23d
Cong Hou <congh@nvidia.com>
2025-08-08 08:15:54 +0300

    Convert the veos file to yaml
    This is to align with community, in the new ansible version, the flow to load ini file is not working anymore.
    
    Change-Id: I15f7cd730a016f5515927bb278524022cb431413

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -428,7 +428,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.449'}
+    default_list = {'docker-ngts': 'ansible_develop-1.2.450'}
     return default_list.get(docker_name, latest)
 
 
