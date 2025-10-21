f63641d29683184d216d1719837b74a59deed778
Yael Tzur <ytzur@nvidia.com>
2025-10-21 18:07:39 +0300

    Update devts tag to 1.2.473
    Change-Id: Iab92abb86d9a89e3809514b64cace570d972f866

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -401,7 +401,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.472'}
+    default_list = {'docker-ngts': '1.2.473'}
     return default_list.get(docker_name, latest)
 
 
