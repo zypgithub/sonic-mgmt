beaf7ae2be81789237d2e9f1a89ed8256d62dbdf
Illia Kotvitskyi <ikotvitskyi@nvidia.com>
2025-10-29 14:41:06 +0200

    devts tag update 1.2.478 -> 1.2.480
    changes:
    * NVOS change for Air
    * revert removing fix for ansible onyx
    
    Change-Id: If224f81e8472e5cde6357039451c96dc1d4f301a

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -401,7 +401,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.478'}
+    default_list = {'docker-ngts': '1.2.480'}
     return default_list.get(docker_name, latest)
 
 
