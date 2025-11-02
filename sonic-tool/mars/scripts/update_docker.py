3889ec7a0dddc0b24a32fd582b5ec258a5349228
Oded Rapaport <orapaport@nvidia.com>
2025-11-02 13:37:54 +0200

    Update devts tag to 1.2.481
    Change-Id: I2ba09c77d4de4f6a63894bb2a8b4c401592994a9

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -401,7 +401,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.2.480'}
+    default_list = {'docker-ngts': '1.2.481'}
     return default_list.get(docker_name, latest)
 
 
