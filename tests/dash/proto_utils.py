35d93f3da797e6ca1a05af2c36acb76ac77e36f8
Cong Hou <congh@nvidia.com>
2025-11-06 09:18:27 +0200

    Add back code which was removed by mistake
    They were removed by mistake when merging back the PR:  https://github.com/sonic-net/sonic-mgmt/pull/19700/
    CR: https://git-nbu-sw.nvidia.com/r/c/switchx/sonic/sonic-mgmt/+/313530
    
    Change-Id: I1b7517e60c017413706c2e3438801ac20ed744a8

--- tests/dash/proto_utils.py
+++ tests/dash/proto_utils.py
@@ -20,6 +20,13 @@ from dash_api.route_rule_pb2 import RouteRule
 
 from google.protobuf.descriptor import FieldDescriptor
 from google.protobuf.json_format import ParseDict
+from dash_api.types_pb2 import IpPrefix, IpVersion, ValueOrRange, IpAddress
+from dash_api.qos_pb2 import Qos
+from dash_api.acl_group_pb2 import AclGroup
+from dash_api.acl_in_pb2 import AclIn
+from dash_api.acl_out_pb2 import AclOut
+from dash_api.acl_rule_pb2 import AclRule, Action
+from dash_api.pa_validation_pb2 import PaValidation
 
 ENABLE_PROTO = True
 
@@ -48,7 +55,11 @@ PB_CLASS_MAP = {
     "METER_POLICY": MeterPolicy,
     "METER_RULE": MeterRule,
     "TUNNEL": Tunnel,
-    "ROUTE_RULE": RouteRule
+    "ROUTE_RULE": RouteRule,
+    "QOS": Qos,
+    "ACL_GROUP": AclGroup,
+    "ACL_RULE": AclRule,
+    "PA_VALIDATION": PaValidation,
 }
 
 
@@ -253,3 +264,92 @@ def json_to_proto(key: str, proto_dict: dict):
 
     pb = ParseDict(new_dict, message)
     return pb.SerializeToString()
+
+
+def acl_group_from_json(json_obj):
+    pb = AclGroup()
+    pb.guid.value = bytes.fromhex(uuid.UUID(json_obj["guid"]).hex)
+    pb.ip_version = IpVersion.IP_VERSION_IPV4
+    return pb
+
+
+def acl_out_from_json(json_obj):
+    pb = AclOut()
+    pb.v4_acl_group_id = json_obj["acl_group_id"]
+    return pb
+
+
+def acl_in_from_json(json_obj):
+    pb = AclIn()
+    pb.v4_acl_group_id = json_obj["acl_group_id"]
+    return pb
+
+
+def acl_rule_from_json(json_obj):
+    pb = AclRule()
+    pb.priority = int(json_obj["priority"])
+    pb.action = Action.ACTION_DENY if json_obj["action"] == "deny" else Action.ACTION_PERMIT
+    pb.terminating = json_obj["terminating"] == "true"
+    if "src_addr" in json_obj:
+        for addr in json_obj["src_addr"].split(','):
+            net = ipaddress.IPv4Network(addr, False)
+            ip = IpPrefix()
+            ip.ip.ipv4 = socket.htonl(int(net.network_address))
+            ip.mask.ipv4 = socket.htonl(int(net.netmask))
+            pb.src_addr.append(ip)
+    if "dst_addr" in json_obj:
+        for addr in json_obj["dst_addr"].split(','):
+            net = ipaddress.IPv4Network(addr, False)
+            ip = IpPrefix()
+            ip.ip.ipv4 = socket.htonl(int(net.network_address))
+            ip.mask.ipv4 = socket.htonl(int(net.netmask))
+            pb.dst_addr.append(ip)
+    if "src_port" in json_obj:
+        for port in json_obj["src_port"].split(','):
+            vr = ValueOrRange()
+            if "-" not in port:
+                vr.value = int(port)
+            else:
+                vr.range.min = int(port.split('-')[0])
+                vr.range.max = int(port.split('-')[1])
+            pb.src_port.append(vr)
+    if "dst_port" in json_obj:
+        for port in json_obj["dst_port"].split(','):
+            vr = ValueOrRange()
+            if "-" not in port:
+                vr.value = int(port)
+            else:
+                vr.range.min = int(port.split('-')[0])
+                vr.range.max = int(port.split('-')[1])
+            pb.dst_port.append(vr)
+    if "protocol" in json_obj:
+        for proto in json_obj["protocol"].split(','):
+            pb.protocol.append(int(proto))
+    if "src_tag" in json_obj:
+        for tag in json_obj["src_tag"].split(','):
+            pb.src_tag.append(tag)
+    if "dst_tag" in json_obj:
+        for tag in json_obj["dst_tag"].split(','):
+            pb.dst_tag.append(tag)
+    return pb
+
+
+def prefix_tag_from_json(json_obj):
+    pb = PrefixTag()
+    pb.ip_version = IpVersion.IP_VERSION_IPV4
+    for ip_prefix in json_obj["prefix_list"].split(','):
+        net = ipaddress.IPv4Network(ip_prefix, False)
+        ip = IpPrefix()
+        ip.ip.ipv4 = socket.htonl(int(net.network_address))
+        ip.mask.ipv4 = socket.htonl(int(net.netmask))
+        pb.prefix_list.append(ip)
+    return pb
+
+
+def pa_validation_from_json(json_obj):
+    pb = PaValidation()
+    for addr in json_obj["addresses"]:
+        ip = IpAddress()
+        ip.ipv4 = socket.htonl(int(ipaddress.ip_address(addr)))
+        pb.addresses.extend([ip])
+    return pb
