3801616b412be5ee323e4b59f4e27d8287797272
Vinod Kumar <119973184+vikumarks@users.noreply.github.com>
2025-11-17 16:16:33 -0800

    Fixing import issue for fixture is_pfc_enabled (#21313)
    * is_pfc_enabled
    
    Fix pfc cases "import is_pfc_enabled"because of recent changes https://github.com/sonic-net/sonic-mgmt/pull/20783
    
    * missed one file update
    
    Change-Id: Icaca282cbae1f61678444738167730bb1d53f5fa

--- tests/snappi_tests/pfc/warm_reboot/test_pfc_pause_lossless_warm_reboot.py
+++ tests/snappi_tests/pfc/warm_reboot/test_pfc_pause_lossless_warm_reboot.py
@@ -6,13 +6,12 @@ from tests.snappi_tests.files.helper import skip_warm_reboot
 from tests.common.platform.processes_utils import wait_critical_processes
 from tests.snappi_tests.pfc.files.helper import run_pfc_test
 from tests.common.utilities import wait_until
-from tests.common.fixtures.conn_graph_facts import conn_graph_facts, \
-    fanout_graph_facts  # noqa: F401
-from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port, \
-    snappi_api, snappi_testbed_config, is_pfc_enabled, get_snappi_ports, get_snappi_ports_single_dut, \
-    get_snappi_ports_multi_dut, is_snappi_multidut  # noqa: F401
-from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, all_prio_list, lossless_prio_list, \
-    lossy_prio_list  # noqa: F401
+from tests.common.fixtures.conn_graph_facts import conn_graph_facts,\
+    fanout_graph_facts                      # noqa: F401
+from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi_api_serv_port,\
+    snappi_api, snappi_testbed_config, is_pfc_enabled       # noqa: F401
+from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, all_prio_list, lossless_prio_list,\
+    lossy_prio_list                         # noqa: F401
 from tests.common.snappi_tests.snappi_test_params import SnappiTestParams
 
 logger = logging.getLogger(__name__)
