395248d78b5412583ed7ba22a787c459e2227e4a
Xixue Jia <xixuej@nvidia.com>
2025-11-21 17:19:06 +0800

    Add DPU/NPU port filtering in PFC counters test
    Enhanced the PFC counters test by incorporating a new utility function to filter out DPU ports from the list of active physical interfaces. This change ensures that only relevant interfaces are considered during the test execution.
    
    Change-Id: I50afcdc8a77773230b4ecf560bed412c7e15e992

--- tests/qos/test_pfc_counters.py
+++ tests/qos/test_pfc_counters.py
@@ -1,4 +1,5 @@
 from tests.common.fixtures.conn_graph_facts import conn_graph_facts, enum_fanout_graph_facts     # noqa: F401
+from tests.common.platform.interface_utils import get_dpu_npu_ports_from_hwsku    # noqa: F401
 from .qos_fixtures import leaf_fanouts      # noqa: F401
 from .qos_helpers import eos_to_linux_intf, nxos_to_linux_intf, sonic_to_linux_intf
 import os
@@ -86,10 +87,12 @@ def run_test(fanouthosts, duthost, conn_graph_facts, enum_fanout_graph_facts, le
     int_status = asic.show_interface(command="status")[
         'ansible_facts']['int_status']
     """ We only test active physical interfaces """
+    dpus = get_dpu_npu_ports_from_hwsku(duthost)
     active_phy_intfs = [intf for intf in int_status if
                         intf.startswith('Ethernet') and
                         int_status[intf]['admin_state'] == 'up' and
-                        int_status[intf]['oper_state'] == 'up']
+                        int_status[intf]['oper_state'] == 'up' and
+                        intf not in dpus]
     only_lossless_rx_counters = "Cisco-8122" in asic.sonichost.facts["hwsku"]
     no_xon_counters = "Cisco-8122" in asic.sonichost.facts["hwsku"]
     if only_lossless_rx_counters and asic_type != 'vs':
