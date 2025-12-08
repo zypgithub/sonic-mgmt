03c73b74838cd08887c3236834a42d16113547a5
Wei Guo <weguo@nvidia.com>
2025-12-08 18:13:20 +0800

    Add packet trimming case in set1 regression
    Change-Id: Ia986184c684abd8ecd180fb4387fe634e77d51dc
    (cherry picked from commit 1a189be5e0ba081f4c5732cc8c32f8c93d6028eb)

--- ngts/scripts/MARS_regression/db_lists_for_nightly_regression.py
+++ ngts/scripts/MARS_regression/db_lists_for_nightly_regression.py
@@ -18,6 +18,7 @@ COMMUNITY_SET1_TEST_GROUP_MAP = {
     'community/common.db': PLATFORM_DEPENDENT,
     'community/snmp.db': PLATFORM_AGNOSTIC_GROUP1,
     'community/wjh.db': PLATFORM_AGNOSTIC_GROUP2,
+    'community/packet_trimming.db': PLATFORM_DEPENDENT,
     'community/warm_reboot.db': PLATFORM_DEPENDENT,
     'community/span.db': PLATFORM_DEPENDENT,
     'community/fast_reboot.db': PLATFORM_DEPENDENT,
