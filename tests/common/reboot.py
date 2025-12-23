29db798bfaf198af99d8f3a0ef4e306661998d0c
Cong Hou <congh@nvidia.com>
2025-12-23 10:18:34 +0200

    Stabilize test_reload_dpu.py
    Enhancement is for the switch memory exhaustion and kernel panic test
    cases.
    
    1. Increase DUT_ABSENT_TIMEOUT_FOR_MEMORY_EXHAUSTION as it takes time to
       exhaust the memory.
    2. Fail the test if the wait_for ssh shutdown fails.
    3. Use a shorter retry delay in the common method
       ssh_connection_with_retry. The check happens after the delay, if the
    delay is bigger than the timeout, this wait_for always fails. See
    ansible document:
    https://docs.ansible.com/projects/ansible/latest/collections/ansible/builtin/wait_for_module.html.
    
    Change-Id: I27ccf446566a6327da5ff4b7363a7b9d6510cb12
    Signed-off-by: Cong Hou <congh@nvidia.com>

--- tests/common/reboot.py
+++ tests/common/reboot.py
@@ -198,6 +198,7 @@ def is_ssh_banner_absent(localhost, dut_ip, check_timeout, ssh_port=22):
     )
     return banner_check.is_failed or 'Timeout' in banner_check.get('msg', '')
 
+
 def wait_for_shutdown(duthost, localhost, delay, timeout, reboot_res=None, interval=1, check_timeout=1):
     """
     Wait for DUT to shutdown by checking SSH banner availability.
@@ -770,7 +771,8 @@ def ssh_connection_with_retry(localhost, host_ip, port, delay, timeout):
         'search_regex': SONIC_SSH_REGEX
     }
     short_timeout = 40
-    params_to_update_list = [{}, {'search_regex': None, 'timeout': short_timeout}]
+    short_delay = 10
+    params_to_update_list = [{}, {'search_regex': None, 'timeout': short_timeout, 'delay': short_delay}]
     for num_try, params_to_update in enumerate(params_to_update_list):
         iter_connection_params = default_connection_params.copy()
         iter_connection_params.update(params_to_update)
