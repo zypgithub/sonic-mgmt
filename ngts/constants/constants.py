6f22bd7db110a028211e46a9e50f6f1a42149980
Cong Hou <congh@nvidia.com>
2025-12-30 09:10:45 +0200

    Add a missing comma
    Change-Id: I0278fe15b08b19b37f36af6b44d1fdcadc2192fc

--- ngts/constants/constants.py
+++ ngts/constants/constants.py
@@ -1190,7 +1190,7 @@ class MarsConstants:
         "t0-isolated-d16u16s1", "t0-isolated-d16u16s2", "t0-28", "dualtor", "dualtor-64",
         "dualtor-aa", "t0-isolated-d2u254s1", "t1-isolated-d254u2s1", "dualtor-64-breakout",
         "dualtor-aa-64-breakout", "t0-88-o8c80", "ptp-256", "t1-isolated-d28u1", "t1-isolated-d56u2",
-        "t0-isolated-d32u32s2", "t0-isolated-v6-d32u32s2", "t0-isolated-d2u254s2", "t1-isolated-v6-d56u1-lag", "t1-48-lag"
+        "t0-isolated-d32u32s2", "t0-isolated-v6-d32u32s2", "t0-isolated-d2u254s2", "t1-isolated-v6-d56u1-lag", "t1-48-lag",
         "t0-isolated-d128u128s2", "t0-isolated-d2u510s2", "t1-isolated-d510u2"
     )
     TOPO_ARRAY_DUALTOR = ("dualtor", "dualtor-64", "dualtor-aa", "dualtor-64-breakout", "dualtor-aa-64-breakout")
