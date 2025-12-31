8ba20e5b090e8475a22891045dc3d53667ffc611
Yael Tzur <ytzur@nvidia.com>
2025-12-31 17:54:51 +0200

    Add memory thr for spc6 platform
    Added platform mapping for test_bulk_counter_performance
    
    
    Change-Id: I70addd49ac3169944e21edce2df4b5a41f3f17d6

--- ngts/tests/nightly/cpu_mem/test_bulk_counter_performance.py
+++ ngts/tests/nightly/cpu_mem/test_bulk_counter_performance.py
@@ -22,7 +22,8 @@ AMPLIFY_FACTORY_FOR_SIMX_MEMORY_THR = {
     "x86_64-mlnx_msn4700_simx-r0": 1.25,
     "x86_64-nvidia_sn5600_simx-r0": 1.25,
     "x86_64-nvidia_sn5640_simx-r0": 1.25,
-    "x86_64-nvidia_sn5810_ld_simx-r0": 1.25
+    "x86_64-nvidia_sn5810_ld_simx-r0": 1.25,
+    "x86_64-nvidia_sn6600_simx-r0": 1.25
 }
 
 
