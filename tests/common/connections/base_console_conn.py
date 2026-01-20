603203b4b279b0d0805a80e40f88c67afa8777dc
Carl Flottmann <carlflottmann@gmail.com>
2026-01-20 16:28:27 +1000

    fix: base console conn bug fix (#21966)
    * fix: added kwargs arg to console connection to align with netmiko
    
    Signed-off-by: Carl Flottmann <t-cflottmann@microsoft.com>
    
    * explicitly add delay factor with kwargs
    
    Signed-off-by: Carl Flottmann <t-cflottmann@microsoft.com>
    
    ---------
    
    Change-Id: Iecd91975adf98b72d50b9c713b960ae2bd4184dc
    Signed-off-by: Carl Flottmann <t-cflottmann@microsoft.com>

--- tests/common/connections/base_console_conn.py
+++ tests/common/connections/base_console_conn.py
@@ -76,10 +76,8 @@ class BaseConsoleConn(CiscoBaseConnection):
         # not supported
         pass
 
-    def find_prompt(self, delay_factor=1, pattern=None):
-        if pattern:
-            return super(BaseConsoleConn, self).find_prompt(delay_factor=delay_factor, pattern=pattern)
-        return super(BaseConsoleConn, self).find_prompt(delay_factor=delay_factor)
+    def find_prompt(self, delay_factor=1, **kwargs):
+        return super(BaseConsoleConn, self).find_prompt(delay_factor, **kwargs)
 
     def clear_buffer(self):
         # todo
