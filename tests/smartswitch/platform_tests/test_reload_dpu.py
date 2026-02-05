4ff3c1fd8f3453ff918540c6382417133a0cf12f
Cong Hou <congh@nvidia.com>
2026-02-05 06:24:08 +0200

    Fix RM#4807510
    Check the DPUs are offline before checking they are online.
    Update a xfail.
    
    Change-Id: Ibc0da3c93faac17d0938c3a658f48b47bf94faf3

--- tests/smartswitch/platform_tests/test_reload_dpu.py
+++ tests/smartswitch/platform_tests/test_reload_dpu.py
@@ -12,7 +12,7 @@ from tests.common.reboot import reboot, REBOOT_TYPE_COLD, SONIC_SSH_PORT, SONIC_
 from tests.common.helpers.dut_utils import is_mellanox_devices
 from tests.smartswitch.common.device_utils_dpu import check_dpu_link_and_status,\
     pre_test_check, post_test_switch_check, post_test_dpus_check,\
-    dpus_shutdown_and_check, dpus_startup_and_check,\
+    dpus_shutdown_and_check, dpus_startup_and_check, check_dpus_module_status,\
     num_dpu_modules, check_dpus_are_not_pingable, check_dpus_reboot_cause  # noqa: F401
 from tests.common.platform.device_utils import platform_api_conn, start_platform_api_service  # noqa: F401,F403
 from tests.smartswitch.common.reboot import perform_reboot
@@ -218,6 +218,9 @@ def test_dpu_status_post_dpu_kernel_panic(duthosts, dpuhosts,
 
         logging.info("Starting UP the DPUs")
         dpus_startup_and_check(duthost, dpu_on_list, num_dpu_modules)
+    else:
+        logging.info("Check DPUs are offline")
+        check_dpus_module_status(duthost, dpu_on_list, "off")
 
     logging.info("Executing post test dpu check")
     reboot_cause_pattern = r"reboot|Non-Hardware"
@@ -273,6 +276,9 @@ def test_dpu_check_post_dpu_mem_exhaustion(duthosts, dpuhosts,
 
         logging.info("Starting UP the DPUs")
         dpus_startup_and_check(duthost, dpu_on_list, num_dpu_modules)
+    else:
+        logging.info("Check DPUs are offline")
+        check_dpus_module_status(duthost, dpu_on_list, "off")
 
     logging.info("Executing post test dpu check")
     reboot_cause_pattern = r"reboot|Non-Hardware"
