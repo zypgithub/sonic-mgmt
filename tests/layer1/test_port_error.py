431353a530c4ec39c4d54bb333061a2d19475968
Guy Shemesh <gshemesh@nvidia.com>
2026-02-18 09:30:01 +0200

    Changing test of L/R Fault according to MSFT comments
    https://github.com/sonic-net/sonic-mgmt/pull/21769
    
    Change-Id: I4b7cf2db5f7a041d98f5af182e4e78bb3ade9987

--- tests/layer1/test_port_error.py
+++ tests/layer1/test_port_error.py
@@ -5,10 +5,9 @@ import time
 
 from tests.common.helpers.assertions import pytest_assert
 from tests.common.utilities import skip_release
-from tests.common.platform.transceiver_utils import parse_sfp_eeprom_infos, get_supported_available_optical_interfaces
-from tests.platform_tests.mellanox.software_control_helper import check_sc_sai_attribute_value
+from tests.common.mellanox_data import is_mellanox_device
 from tests.common.utilities import wait_until
-
+from tests.layer1.conftest import TestMACFaultMellanox
 
 pytestmark = [
     pytest.mark.disable_loganalyzer,  # disable automatic loganalyzer
@@ -16,31 +15,22 @@ pytestmark = [
 ]
 
 SUPPORTED_PLATFORMS = ["arista_7060x6", "nvidia_sn5640", "nvidia_sn5600"]
+WAIT_FOR_PORT_SHUTDOWN = 5
+WAIT_FOR_PORT_STARTUP = 20
 cmd_sfp_presence = "sudo sfpshow presence"
-DEFAULT_COLLECTED_PORTS_NUM = 5
 
-def pytest_addoption(parser):
-    """
-    Add command line options for pytest
-    """
-    parser.addoption(
-        "--collected-ports-num",
-        action="store",
-        default=DEFAULT_COLLECTED_PORTS_NUM,
-        type=int,
-        help="Number of ports to collect for testing (default: {})".format(DEFAULT_COLLECTED_PORTS_NUM)
-    )
 
 @pytest.fixture(scope="session")
-def collected_ports_num(request):
-    """
-    Fixture to get the number of ports to collect from command line argument
-    """
-    return request.config.getoption("collected_ports_num", default=DEFAULT_COLLECTED_PORTS_NUM)
+def vendor_specific_obj(duthost):
+    if is_mellanox_device(duthost):
+        return TestMACFaultMellanox()
+    else:
+        pytest.skip("Test is not implemented for this specific vendor")
+
 
 class TestMACFault(object):
-    @pytest.fixture(autouse=True)
-    def is_supported_platform(self, duthost, tbinfo):
+    @pytest.fixture(scope="class", autouse=True)
+    def is_supported_platform(self, duthost, tbinfo, vendor_specific_obj):
         if 'ptp' not in tbinfo['topo']['name']:
             pytest.skip("Skipping test: Not applicable for non-PTP topology")
 
@@ -49,9 +39,8 @@ class TestMACFault(object):
         else:
             pytest.skip("DUT has platform {}, test is not supported".format(duthost.facts['platform']))
 
-        if 'nvidia' in duthost.facts['platform'].lower() and not check_sc_sai_attribute_value(duthost):
-            pytest.skip("SW control feature is not enabled on platform")
-
+        if not vendor_specific_obj.is_setting_support_feature(duthost):
+            pytest.skip("Platform setting is not supported for this test")
 
     @staticmethod
     def get_mac_fault_count(dut, interface, fault_type):
@@ -72,46 +61,45 @@ class TestMACFault(object):
         return dut.show_and_parse("show interfaces status {}".format(interface))[0].get("oper", "unknown")
 
     @pytest.fixture(scope="class", autouse=True)
-    def reboot_dut(self, duthosts, localhost, enum_rand_one_per_hwsku_frontend_hostname):
+    def reboot_dut(self, rand_selected_dut, localhost):
         from tests.common.reboot import reboot
-        reboot(duthosts[enum_rand_one_per_hwsku_frontend_hostname],
+        reboot(rand_selected_dut,
                localhost, safe_reboot=True, check_intf_up_ports=True)
 
     @pytest.fixture(scope="class")
-    def select_random_interfaces(self, duthosts, enum_rand_one_per_hwsku_frontend_hostname, collected_ports_num):
-        dut = duthosts[enum_rand_one_per_hwsku_frontend_hostname]
+    def select_random_interfaces(self, rand_selected_dut, vendor_specific_obj, collected_ports_num):
+        dut = rand_selected_dut
 
         sfp_presence = dut.command(cmd_sfp_presence)
         parsed_presence = {line.split()[0]: line.split()[1] for line in sfp_presence["stdout_lines"][2:]}
+        test_available_ifaces = []
+        failed_api_ports = []
 
-        eeprom_infos = dut.command("sudo sfputil show eeprom -d")['stdout']
-        eeprom_infos = parse_sfp_eeprom_infos(eeprom_infos)
+        test_available_ifaces, failed_api_ports = (
+            vendor_specific_obj.return_available_interfaces(dut, parsed_presence)
+        )
 
+        test_ifaces = random.sample(test_available_ifaces,
+                                    min(collected_ports_num, len(test_available_ifaces)))
 
-        available_optical_interfaces = get_supported_available_optical_interfaces(eeprom_infos, parsed_presence)
+        return test_ifaces, failed_api_ports
 
-        pytest_assert(available_optical_interfaces, "No interfaces with SFP detected. Cannot proceed with tests.")
-        logging.info("Available Optical interfaces for tests: {}".format(available_optical_interfaces))
-
-        selected_interfaces = random.sample(available_optical_interfaces, min(collected_ports_num, len(available_optical_interfaces)))
-        logging.info("Selected interfaces for tests: {}".format(selected_interfaces))
-
-        return dut, selected_interfaces
-
-    def shutdown_and_startup_interfaces(self, dut, interface):
+    def toggle_iface(self, dut, interface):
         dut.command("sudo config interface shutdown {}".format(interface))
-        pytest_assert(wait_until(30, 2, 0, lambda: self.get_interface_status(dut, interface) == "down"),
-                     "Interface {} did not go down after shutdown".format(interface))
+        pytest_assert(wait_until(10, 1, 0, lambda: self.get_interface_status(dut, interface) == "down"),
+                      "Interface {} did not go down after shutdown".format(interface))
 
         dut.command("sudo config interface startup {}".format(interface))
-        pytest_assert(wait_until(30, 2, 0, lambda: self.get_interface_status(dut, interface) == "up"),
-                     "Interface {} did not come up after startup".format(interface))
+        pytest_assert(wait_until(30, 1, 0, lambda: self.get_interface_status(dut, interface) == "up"),
+                      "Interface {} did not come up after startup".format(interface))
 
-    def test_mac_local_fault_increment(self, select_random_interfaces):
-        dut, interfaces = select_random_interfaces
+    def test_mac_local_fault_increment(self, select_random_interfaces, rand_selected_dut):
+        dut = rand_selected_dut
+        selected_ifaces, failed_api_ports = select_random_interfaces
+        logging.info("Selected interfaces for tests: {}".format(selected_ifaces))
 
-        for interface in interfaces:
-            self.shutdown_and_startup_interfaces(dut, interface)
+        for interface in selected_ifaces:
+            self.toggle_iface(dut, interface)
 
             pytest_assert(self.get_interface_status(dut, interface) == "up",
                           "Interface {} was not up before disabling/enabling rx-output using sfputil".format(interface))
@@ -120,13 +108,13 @@ class TestMACFault(object):
             logging.info("Initial MAC local fault count on {}: {}".format(interface, local_fault_before))
 
             dut.shell("sudo sfputil debug rx-output {} disable".format(interface))
-            time.sleep(5)
+            time.sleep(WAIT_FOR_PORT_SHUTDOWN)
             pytest_assert(self.get_interface_status(dut, interface) == "down",
                           "Interface {iface} did not go down after 'sudo sfputil debug rx-output {iface} disable'"
                           .format(iface=interface))
 
             dut.shell("sudo sfputil debug rx-output {} enable".format(interface))
-            time.sleep(20)
+            time.sleep(WAIT_FOR_PORT_STARTUP)
             pytest_assert(self.get_interface_status(dut, interface) == "up",
                           "Interface {iface} did not come up after 'sudo sfputil debug rx-output {iface} enable'"
                           .format(iface=interface))
@@ -138,11 +126,15 @@ class TestMACFault(object):
             pytest_assert(local_fault_after > local_fault_before,
                           "MAC local fault count did not increment after disabling/enabling rx-output on the device")
 
-    def test_mac_remote_fault_increment(self, select_random_interfaces):
-        dut, interfaces = select_random_interfaces
+        pytest_assert(len(failed_api_ports) == 0, "Interfaces with failed API ports: {}".format(failed_api_ports))
 
-        for interface in interfaces:
-            self.shutdown_and_startup_interfaces(dut, interface)
+    def test_mac_remote_fault_increment(self, select_random_interfaces, rand_selected_dut):
+        dut = rand_selected_dut
+        selected_ifaces, failed_api_ports = select_random_interfaces
+        logging.info("Selected interfaces for tests: {}".format(selected_ifaces))
+
+        for interface in selected_ifaces:
+            self.toggle_iface(dut, interface)
 
             pytest_assert(self.get_interface_status(dut, interface) == "up",
                           "Interface {} was not up before disabling/enabling tx-output using sfputil".format(interface))
@@ -151,13 +143,13 @@ class TestMACFault(object):
             logging.info("Initial MAC remote fault count on {}: {}".format(interface, remote_fault_before))
 
             dut.shell("sudo sfputil debug tx-output {} disable".format(interface))
-            time.sleep(5)
+            time.sleep(WAIT_FOR_PORT_SHUTDOWN)
             pytest_assert(self.get_interface_status(dut, interface) == "down",
                           "Interface {iface} did not go down after 'sudo sfputil debug tx-output {iface} disable'"
                           .format(iface=interface))
 
             dut.shell("sudo sfputil debug tx-output {} enable".format(interface))
-            time.sleep(20)
+            time.sleep(WAIT_FOR_PORT_STARTUP)
 
             pytest_assert(self.get_interface_status(dut, interface) == "up",
                           "Interface {iface} did not come up after 'sudo sfputil debug tx-output {iface} enable'"
@@ -169,3 +161,5 @@ class TestMACFault(object):
 
             pytest_assert(remote_fault_after > remote_fault_before,
                           "MAC remote fault count did not increment after disabling/enabling tx-output on the device")
+
+        pytest_assert(len(failed_api_ports) == 0, "Interfaces with failed API ports: {}".format(failed_api_ports))
