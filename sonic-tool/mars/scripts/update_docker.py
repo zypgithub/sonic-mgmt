4097028490dc7e11456e65e5dd5b8a21fc97b8b8
Aharon Malkin <amalkin@nvidia.com>
2026-03-17 18:44:02 +0200

    Update NGTS docker tag to 1.3.28
    Change-Id: I686a0251d6a867c4cc92ba920d63b5cb73c52dda
    (cherry picked from commit 30b1ba23f26224d4d116aaaa6b08fd6846a96882)

--- sonic-tool/mars/scripts/update_docker.py
+++ sonic-tool/mars/scripts/update_docker.py
@@ -413,7 +413,7 @@ def main():
 
 def get_docker_default_tag(docker_name):
     latest = "latest"
-    default_list = {'docker-ngts': '1.3.22'}
+    default_list = {'docker-ngts': '1.3.28'}
     return default_list.get(docker_name, latest)
 
 
