420da7d785b7df5c57388ac02331f692e056be14
Anilal Pathiyappara <apathiya@amd.com>
2026-03-20 13:39:35 -0700

    [Smartswitch]: [DPU]: DASH Private Link Redirect Feature (#22367)
    * Feature : Private link Redirect
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Feature : Private link Redirect Test Plan
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Feature : Private link Redirect
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Feature : Private link Redirect
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Feature : Feature, Private Link Redirect
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Feature : Private Link Redirect, skip test on kvm & non-smartswitch
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Feature : Private Link Redirect, New Changes
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Spelling correction “tranformed” -> “transformed”.
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    * Feature Private Link Redirect : Copilot Review Changes
    
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>
    
    ---------
    
    Change-Id: Iade5458a11d01ee7c794d550e184c4ef3c929848
    Signed-off-by: Anilal Pathiyappara <apathiya@amd.com>

--- tests/dash/proto_utils.py
+++ tests/dash/proto_utils.py
@@ -17,6 +17,8 @@ from dash_api.meter_policy_pb2 import MeterPolicy
 from dash_api.meter_rule_pb2 import MeterRule
 from dash_api.tunnel_pb2 import Tunnel
 from dash_api.route_rule_pb2 import RouteRule
+from dash_api.outbound_port_map_pb2 import OutboundPortMap
+from dash_api.outbound_port_map_range_pb2 import OutboundPortMapRange
 
 from google.protobuf.descriptor import FieldDescriptor
 from google.protobuf.json_format import ParseDict
@@ -56,10 +58,8 @@ PB_CLASS_MAP = {
     "METER_RULE": MeterRule,
     "TUNNEL": Tunnel,
     "ROUTE_RULE": RouteRule,
-    "QOS": Qos,
-    "ACL_GROUP": AclGroup,
-    "ACL_RULE": AclRule,
-    "PA_VALIDATION": PaValidation,
+    "OUTBOUND_PORT_MAP_RANGE": OutboundPortMapRange,
+    "OUTBOUND_PORT_MAP": OutboundPortMap
 }
 
 
