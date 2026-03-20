a02bf8acef7401c0888558e149d60e464519f6f7
Sviatoslav Demniak <sviatoslavd@mellanox.com>
2026-03-20 09:27:42 +0200

    fix for platform test
    Change-Id: I8911ac31c69d71f2c0c38081122d41e262556c25

--- ngts/tests_nvos/platform/test_platform_transceiver.py
+++ ngts/tests_nvos/platform/test_platform_transceiver.py
@@ -141,7 +141,7 @@ def test_transceiver_status_with_reboot(engines, devices, random_api):
 @pytest.mark.platform
 @pytest.mark.transceiver
 @pytest.mark.parametrize('test_api', [ApiType.NVUE])
-def test_transceivers_and_ports(engines, devices, nv_command, random_api):
+def test_transceivers_and_ports(engines, devices, nv_command, random_api, test_api):
     """
     The test verifies all expected modules (by device) exists in transceivers detail output.
 
