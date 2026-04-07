efa5902343935c4576cb9f79487838aad50dd99e
Elias Abboud <eabboud@nvidia.com>
2026-04-07 12:55:59 +0300

    rbac-mode is disabled by default
    Change-Id: I08e891fd6ae0da3169cc1acff8cc695bc6d97b70

--- ngts/nvos_tools/Devices/IbDevice.py
+++ ngts/nvos_tools/Devices/IbDevice.py
@@ -1771,7 +1771,7 @@ class JulietScaleoutSwitch(JulietSwitch):
                 },
                 "rbac": {
                     "rbac-file": "",
-                    "rbac-mode": ""
+                    "rbac-mode": "disabled"
                 }
             },
             ClusterConsts.NMX_TELEMETRY: {
@@ -1789,7 +1789,7 @@ class JulietScaleoutSwitch(JulietSwitch):
                 },
                 "rbac": {
                     "rbac-file": "",
-                    "rbac-mode": ""
+                    "rbac-mode": "disabled"
                 }
             }
         }
@@ -2436,12 +2436,12 @@ class RosalindSurrogateSwitch(JulietNonScaleoutSwitch):
             ClusterConsts.NMX_CONTROLLER: {
                 **{key: value for key, value in self.cluster_app_nmx_controller.items() if key not in []},
                 'manager': {"ca-certificate": "", "certificate": "", "crl": "", "encryption": "disabled", "state": "disabled"},
-                "rbac": {"rbac-file": "", "rbac-mode": ""}
+                "rbac": {"rbac-file": "", "rbac-mode": "disabled"}
             },
             ClusterConsts.NMX_TELEMETRY: {
                 **{key: value for key, value in self.cluster_app_nmx_telemetry.items() if key not in []},
                 'manager': {"ca-certificate": "", "certificate": "", "crl": "", "encryption": "disabled", "state": "enabled"},
-                "rbac": {"rbac-file": "", "rbac-mode": ""}
+                "rbac": {"rbac-file": "", "rbac-mode": "disabled"}
             }
         }
 
