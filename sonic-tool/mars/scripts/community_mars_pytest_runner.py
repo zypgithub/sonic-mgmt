96f3ad9ce8641163a634f9327bd88d184c7e7c23
jbao <jbao@nvidia.com>
2026-04-16 11:49:10 +0300

    When self.setup_name exists, use setup_name to generate allure project, otherwise use MARS_SETUP_ID to generate it.
    Change-Id: I15271fd308b35be61d2baf48ab345ff0b7869ffc

--- sonic-tool/mars/scripts/community_mars_pytest_runner.py
+++ sonic-tool/mars/scripts/community_mars_pytest_runner.py
@@ -223,7 +223,9 @@ class RunPytest(TermHandlerMixin, StandaloneWrapper):
         if '--allure_server_project_id' in self.raw_options:
             allure_proj_pytest_arg = ''
         else:
-            allure_proj = get_allure_project_id(os.environ.get('MARS_SETUP_ID', self.setup_name), self.test_scripts, get_dut_name_only=False)
+            setup_id = self.setup_name or os.environ.get('MARS_SETUP_ID')
+            self.Logger.info(f"The setup id is {setup_id}")
+            allure_proj = get_allure_project_id(setup_id, self.test_scripts, get_dut_name_only=False)
             allure_proj_pytest_arg = '--allure_server_project_id={}'.format(allure_proj)
 
         # If the test case contains a topology mark, add --topology parameter to the pytest raw option
@@ -362,7 +364,7 @@ class RunPytest(TermHandlerMixin, StandaloneWrapper):
     def collect_allure_report_data(self):
         self.Logger.info('Going to upload allure data to server')
 
-        cmd = 'PYTHONPATH=/devts /ngts_venv/bin/python {}/ngts/scripts/allure_reporter.py --action upload --setup_name {}'.format(self.sonic_mgmt_path, os.environ.get('MARS_SETUP_ID', self.setup_name))
+        cmd = 'PYTHONPATH=/devts /ngts_venv/bin/python {}/ngts/scripts/allure_reporter.py --action upload --setup_name {}'.format(self.sonic_mgmt_path, self.setup_name)
         self.Logger.info('Running cmd: {}'.format(cmd))
         self.EPoints[0].Player.run_process(cmd, shell=True, disable_realtime_log=False, delete_files=False)
 
