08519710abb3e76dbadf6091e3bf24b05f83943f
Illia Kotvitskyi <ikotvitskyi@nvidia.com>
2026-04-23 16:01:22 +0300

    update PLATFORM_MODEL_MAP
    Change-Id: I5f0fa89950eae06e471b0b8a47034593b2280cd2

--- ngts/constants/constants.py
+++ ngts/constants/constants.py
@@ -1804,12 +1804,12 @@ class SerialConsts:
         "x86_64-nvidia_sn6600_ld_simx-r0": "MT25116050YD"
     }
     PLATFORM_MODEL_MAP = {
-        "x86_64-mlnx_msn2700_simx-r0": "MSN2700",
-        "x86_64-mlnx_msn4700_simx-r0": "MSN4700",
+        "x86_64-mlnx_msn2700_simx-r0": "MSN2700-CS2FO",
+        "x86_64-mlnx_msn4700_simx-r0": "MSN4700-WS2FO",
         "x86_64-nvidia_sn5600_simx-r0": "SN5600",
-        "x86_64-nvidia_sn5640_simx-r0": "SN5640",
+        "x86_64-nvidia_sn5640_simx-r0": "MSN5640",
         "x86_64-nvidia_sn5800_ld_simx-r0": "SN5800_LD",
-        "x86_64-nvidia_sn6600_simx-r0": "SN6600",
+        "x86_64-nvidia_sn6600_simx-r0": "SN6600_LD",
         "x86_64-nvidia_sn6600_ld_simx-r0": "SN6600_LD"
     }
 
