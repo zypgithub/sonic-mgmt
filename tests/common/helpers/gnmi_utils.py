ab923615834e2c60d1d93864c510001aa1a9cb00
Wenjun Wang <wenjwang@nvidia.com>
2026-05-13 12:17:04 +0300

    generate gnmi crl based on dut ip family
    Change-Id: I2e68ae6f2a92fa5b76904b11c3bc4ff3cdc3f557

--- tests/common/helpers/gnmi_utils.py
+++ tests/common/helpers/gnmi_utils.py
@@ -188,14 +188,14 @@ def get_ptf_crl_server_ip(duthost, ptfhost):
         return ptfhost.mgmt_ip
 
 
-def create_revoked_cert_and_crl(localhost, ptfhost, duthost=None):
+def create_revoked_cert_and_crl(localhost, ptfhost, duthost):
     create_client_key(localhost, revoke=True)
 
     create_client_csr(localhost, revoke=True)
 
     # Sign client certificate
     # Get appropriate PTF IP address based on DUT management IP type
-    ptf_ip = get_ptf_crl_server_ip(duthost, ptfhost) if duthost else ptfhost.mgmt_ip
+    ptf_ip = get_ptf_crl_server_ip(duthost, ptfhost)
     crl_url = "http://{}:1234/crl".format(ptf_ip)
     create_ca_conf(crl_url, "crlext.cnf")
     sign_client_certificate(localhost, revoke=True, extension_file="crlext.cnf")
@@ -250,7 +250,7 @@ def create_gnmi_certs(duthost, localhost, ptfhost):
     prepare_root_cert(localhost)
     prepare_server_cert(duthost, localhost)
     prepare_client_cert(localhost)
-    create_revoked_cert_and_crl(localhost, ptfhost)
+    create_revoked_cert_and_crl(localhost, ptfhost, duthost)
     copy_certificate_to_dut(duthost)
     copy_certificate_to_ptf(ptfhost)
 
