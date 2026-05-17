ec3562c1c9d41d17ed76be994f4a66db4fd7efee
AntonHryshchuk <antonh@nvidia.com>
2026-05-17 12:34:27 +0300

    Fix deploy of slm-16 setup
    Change-Id: I74d9b3fc951acb4ec7a83e5f618576312d180350

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -651,23 +651,24 @@ def get_port_alias_to_name_map(hwsku, asic_name=None):
                         port_alias_to_name_map[alias] = eth_name
             port_alias_to_name_map['etp65'] = "Ethernet512"
             port_alias_to_name_map['etp66'] = "Ethernet520"
-        elif hwsku in ["ACS-SN6600", "Mellanox-SN6600-C512S4"]:
-            if hwsku in ["ACS-SN6600"]:
+        elif hwsku in ["ACS-SN6600", "Mellanox-SN6600-C512S4", "Mellanox-SN6600_LD-P128C2"]:
+            if hwsku in ["ACS-SN6600", "Mellanox-SN6600_LD-P128C2"]:
                 split_alias_list = ["a", "b"]
             else:
                 split_alias_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
             for i in range(1, 65):
                 for idx, split_alias in enumerate(split_alias_list):
                     alias = "etp{}{}".format(i, split_alias)
-                    if hwsku in ["ACS-SN6600"]:
+                    if hwsku in ["ACS-SN6600", "Mellanox-SN6600_LD-P128C2"]:
                         eth_name = "Ethernet{}".format((i - 1) * 8 + idx * 4)
                     else:
                         eth_name = "Ethernet{}".format((i - 1) * 8 + idx)
                     port_alias_to_name_map[alias] = eth_name
             port_alias_to_name_map['etp65a'] = "Ethernet512"
             port_alias_to_name_map['etp65b'] = "Ethernet513"
-            port_alias_to_name_map['etp66'] = "Ethernet520"
-            port_alias_to_name_map['etp67'] = "Ethernet528"
+            if hwsku != "Mellanox-SN6600_LD-P128C2":
+                port_alias_to_name_map['etp66'] = "Ethernet520"
+                port_alias_to_name_map['etp67'] = "Ethernet528"
         elif hwsku in ["Mellanox-SN6600_LD-V512C2", "Mellanox-SN6600_LD-V448P16C2"]:
             split_alias_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
             for i in range(1, 65):
--- ngts/scripts/sonic_deploy/sonic_only_methods.py
+++ ngts/scripts/sonic_deploy/sonic_only_methods.py
@@ -178,7 +178,8 @@ class SonicInstallationSteps:
         if (not is_dualtor_topo(sonic_topo) and 'bobcat' not in dut_name and "r-moose-01" != dut_name and
                 "mtvr-moose-04" != dut_name and "r-leopard-01" != dut_name and "r-leopard-58" != dut_name and
                 'r-tigon-04' != dut_name and "mtvr-moose-13" != dut_name and "mtvr-moose-14" != dut_name and
-                "mtvr-gaur-02" != dut_name and "mtvr-gaur-03" != dut_name and "air-6600" not in dut_name and not setup_name.endswith('-ha')):
+                "mtvr-gaur-02" != dut_name and "mtvr-gaur-03" != dut_name and "air-6600" not in dut_name and
+                not dut_name.startswith('slm-') and not setup_name.endswith('-ha')):
             gen_mg_cmd = get_generate_minigraph_cmd(setup_info, dut_name, sonic_topo, port_number)
             run_background_process_on_host(threads_dict, 'generate_minigraph', gen_mg_cmd, timeout=300,
                                            exec_path=ansible_path, deploy_sequential=deploy_sequential)
