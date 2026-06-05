7023895ef2411f405b35d985a4249ff65bcc44e9
mramezani95 <mramezani@microsoft.com>
2026-06-05 15:36:10 -0700

    Skipping GCU packet trimming tests on non-supported SKUs (#24568)
    <!--
    Please make sure you've read and understood our contributing guidelines;
    https://github.com/sonic-net/SONiC/blob/gh-pages/CONTRIBUTING.md
    
    Please provide following information to help code review process a bit
    easier:
    -->
    ### Description of PR
    <!--
    - Please include a summary of the change and which issue is fixed.
    - Please also include relevant motivation and context. Where should
    reviewer start? background context?
    - List any dependencies that are required for this change.
    -->
    This PR skips packet-trimming GCU config tests on non-supported SKUs.
    This is done by moving these tests to the `packet_trimming/` directory.
    
    Summary:
    Microsoft ADO ID: 37430984
    Skipping GCU packet trimming tests on non-supported SKUs
    
    ---------
    
    Change-Id: Id7a4da0c95b29073c9b08e957a7ffc35942692f4
    Signed-off-by: Mahdi Ramezani <mramezani@microsoft.com>

--- tests/packet_trimming/conftest.py
+++ tests/packet_trimming/conftest.py
@@ -167,7 +167,7 @@ def queue_level_trim_supported(duthost):
 
 
 @pytest.fixture(scope="module", autouse=True)
-def setup_trimming(duthost, test_params, trim_counter_params):
+def setup_trimming(duthost, test_params, trim_counter_params, request):
     """
     Set up all prerequisites for packet trimming tests.
 
@@ -176,6 +176,12 @@ def setup_trimming(duthost, test_params, trim_counter_params):
         test_params: Test parameters from test_params fixture
         trim_counter_params: Test parameters from trim_counter_params fixture (for counter tests)
     """
+    if request.module.__name__ == "tests.packet_trimming.test_packet_trimming_config_symmetric" or \
+       request.module.__name__ == "tests.packet_trimming.test_packet_trimming_config_asymmetric":
+        # Skip the setup for GCU config tests.
+        yield
+        return
+
     logger.info("Prepare packet trimming related configurations")
     platform = duthost.facts['platform']
 
@@ -344,10 +350,15 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="function", autouse=True)
-def clear_counters(duthost):
+def clear_counters(duthost, request):
     """
     Clear all counters on the DUT.
     """
+    if request.module.__name__ == "tests.packet_trimming.test_packet_trimming_config_symmetric" or \
+       request.module.__name__ == "tests.packet_trimming.test_packet_trimming_config_asymmetric":
+        # Skip for GCU config tests.
+        return
+
     duthost.shell("sonic-clear counters")
     duthost.shell("sonic-clear queuecounters")
     duthost.shell("sonic-clear switchcounters")
